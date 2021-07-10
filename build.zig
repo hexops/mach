add19e2577e660821b5c9774e3780935f23f30e0
Stephen Gutekanst <stephen@hexops.com>
2021-07-10 14:47:32 -0700

    add -Dtarget option
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -3,16 +3,20 @@ const glfw = @import("glfw/build.zig");
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
+
     const lib = b.addStaticLibrary("engine", "src/main.zig");
-    lib.addPackagePath("glfw", "glfw/src/main.zig");
     lib.setBuildMode(mode);
+    lib.setTarget(target);
+    lib.addPackagePath("glfw", "glfw/src/main.zig");
     glfw.link(b, lib, .{});
     lib.install();
 
     var main_tests = b.addTest("src/main.zig");
+    main_tests.setBuildMode(mode);
+    main_tests.setTarget(target);
     main_tests.addPackagePath("glfw", "glfw/src/main.zig");
     glfw.link(b, main_tests, .{});
-    main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
