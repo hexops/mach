1eeb049b5c122ba22be8a0d70dd0afdd75dc96db
Stephen Gutekanst <stephen@hexops.com>
2021-10-16 17:24:36 -0700

    glfw: add vulkan headers
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- glfw/update-upstream.sh
+++ glfw/update-upstream.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
+# GLFW
 rm -rf upstream/
 mkdir upstream/
 cd upstream/
@@ -13,3 +14,10 @@ rm -rf .appveyor.yml .git .gitattributes .gitignore .mailmap .travis.yml
 rm cmake_uninstall.cmake.in README.md
 rm -r CMake* deps/ examples/ tests/ docs/
 rm src/CMakeLists.txt src/*.in
+
+# Vulkan headers
+cd ..
+git clone https://github.com/KhronosGroup/Vulkan-Headers vulkan_headers/
+cd vulkan_headers
+rm -rf .git registry/ *.gn *.txt *.md cmake/ 
+rm -rf include/vk_video
