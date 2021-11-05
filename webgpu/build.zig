0391c1f3f08a398c3817291f4865e1af6c8fbbc4
Stephen Gutekanst <stephen@hexops.com>
2021-11-05 03:32:34 -0700

    webgpu: begin building Dawn with Zig
    * MacOS support functional
    * C shims for dawn_native functionality, e.g. required utilities for binding to a GLFW window.
    * Fully building with Zig, but depends on ziglang/zig#10096 for Obj-C++ support.
    
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- webgpu/build.zig
+++ webgpu/build.zig
@@ -1,14 +1,38 @@
 const Builder = @import("std").build.Builder;
+const dawn = @import("build_dawn.zig");
+
+const glfw = @import("./libs/dawn/mach-old/glfw/build.zig"); // TODO: mach-old path
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
+
     const lib = b.addStaticLibrary("webgpu", "src/main.zig");
     lib.setBuildMode(mode);
+    lib.setTarget(target);
     lib.install();
+    dawn.link(b, lib, .{});
 
     var main_tests = b.addTest("src/main.zig");
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+
+    const dawn_example = b.addExecutable("dawn-example", "src/dawn/hello_triangle.zig");
+    dawn_example.setBuildMode(mode);
+    dawn_example.setTarget(target);
+    dawn.link(b, dawn_example, .{});
+    glfw.link(b, dawn_example, .{.system_sdk = .{.set_sysroot = false}});
+    dawn_example.addPackagePath("glfw", "./libs/dawn/mach-old/glfw/src/main.zig"); // TODO: mach-old path
+    dawn_example.addIncludeDir("libs/dawn/out/Debug/gen/src/include");
+    dawn_example.addIncludeDir("libs/dawn/out/Debug/gen/src");
+    dawn_example.addIncludeDir("libs/dawn/src/include");
+    dawn_example.addIncludeDir("src/dawn");
+    dawn_example.install();
+
+    const dawn_example_run_cmd = dawn_example.run();
+    dawn_example_run_cmd.step.dependOn(b.getInstallStep());
+    const dawn_example_run_step = b.step("run-dawn-example", "Run the dawn example");
+    dawn_example_run_step.dependOn(&dawn_example_run_cmd.step);
 }
---
+++ webgpu/src/dawn/dawn_native_c.h
@@ -0,0 +1,102 @@
+#ifndef MACH_DAWNNATIVE_C_H_
+#define MACH_DAWNNATIVE_C_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(MACH_DAWNNATIVE_C_SHARED_LIBRARY)
+#    if defined(_WIN32)
+#        if defined(MACH_DAWNNATIVE_C_IMPLEMENTATION)
+#            define MACH_EXPORT __declspec(dllexport)
+#        else
+#            define MACH_EXPORT __declspec(dllimport)
+#        endif
+#    else  // defined(_WIN32)
+#        if defined(MACH_DAWNNATIVE_C_IMPLEMENTATION)
+#            define MACH_EXPORT __attribute__((visibility("default")))
+#        else
+#            define MACH_EXPORT
+#        endif
+#    endif  // defined(_WIN32)
+#else       // defined(MACH_DAWNNATIVE_C_SHARED_LIBRARY)
+#    define MACH_EXPORT
+#endif  // defined(MACH_DAWNNATIVE_C_SHARED_LIBRARY)
+
+#include <dawn/webgpu.h>
+#include <dawn/dawn_proc_table.h>
+
+// TODO(slimsag): future: Dawn authors want dawn_native::AdapterProperties to eventually be in webgpu.h,
+// and there is a corresponding WGPUAdapterProperties struct today, but there aren't corresponding methods
+// to actually use / work with it today.
+typedef struct MachDawnNativeAdapterPropertiesImpl* MachDawnNativeAdapterProperties;
+
+MACH_EXPORT void machDawnNativeAdapterProperties_deinit(MachDawnNativeAdapterProperties properties);
+MACH_EXPORT uint32_t machDawnNativeAdapterProperties_getVendorID(MachDawnNativeAdapterProperties properties);
+MACH_EXPORT uint32_t machDawnNativeAdapterProperties_getDeviceID(MachDawnNativeAdapterProperties properties);
+MACH_EXPORT char const* machDawnNativeAdapterProperties_getName(MachDawnNativeAdapterProperties properties);
+MACH_EXPORT char const* machDawnNativeAdapterProperties_getDriverDescription(MachDawnNativeAdapterProperties properties);
+MACH_EXPORT WGPUAdapterType machDawnNativeAdapterProperties_getAdapterType(MachDawnNativeAdapterProperties properties);
+MACH_EXPORT WGPUBackendType machDawnNativeAdapterProperties_getBackendType(MachDawnNativeAdapterProperties properties);
+
+// An adapter is an object that represent on possibility of creating devices in the system.
+// Most of the time it will represent a combination of a physical GPU and an API. Not that the
+// same GPU can be represented by multiple adapters but on different APIs.
+//
+// The underlying Dawn adapter is owned by the Dawn instance so this is just a reference to an
+// underlying adapter.
+typedef struct MachDawnNativeAdapterImpl* MachDawnNativeAdapter;
+
+MACH_EXPORT MachDawnNativeAdapterProperties machDawnNativeAdapter_getProperties(MachDawnNativeAdapter adapter);
+
+// An optional parameter of Adapter::CreateDevice() to send additional information when creating
+// a Device. For example, we can use it to enable a workaround, optimization or feature.
+typedef struct MachDawnNativeDeviceDescriptor {
+    char** requiredExtensions;
+    uintptr_t requiredExtensionsLength;
+
+    char** forceEnabledToggles;
+    uintptr_t forceEnabledTogglesLength;
+
+    char** forceDisabledToggles;
+    uintptr_t forceDisabledTogglesLength;
+
+    // default null
+    WGPURequiredLimits* requiredLimits;
+} MachDawnNativeDeviceDescriptor;
+MACH_EXPORT WGPUDevice machDawnNativeAdapter_createDevice(MachDawnNativeAdapter adapter, MachDawnNativeDeviceDescriptor* deviceDescriptor);
+
+typedef struct MachDawnNativeAdaptersImpl* MachDawnNativeAdapters;
+MACH_EXPORT MachDawnNativeAdapter machDawnNativeAdapters_index(MachDawnNativeAdapters adapters, uintptr_t index);
+MACH_EXPORT uintptr_t machDawnNativeAdapters_length(MachDawnNativeAdapters adapters);
+
+// Represents a connection to dawn_native and is used for dependency injection, discovering
+// system adapters and injecting custom adapters (like a Swiftshader Vulkan adapter).
+//
+// This can be initialized via machDawnNativeInstanceInit and destroyed via
+// machDawnNativeInstanceDeinit. The instance controls the lifetime of all adapters for the
+// instance.
+typedef struct MachDawnNativeInstanceImpl* MachDawnNativeInstance;
+
+MACH_EXPORT MachDawnNativeInstance machDawnNativeInstance_init(void);
+MACH_EXPORT void machDawnNativeInstance_deinit(MachDawnNativeInstance);
+MACH_EXPORT void machDawnNativeInstance_discoverDefaultAdapters(MachDawnNativeInstance);
+MACH_EXPORT MachDawnNativeAdapters machDawnNativeInstance_getAdapters(MachDawnNativeInstance instance);
+
+// Backend-agnostic API for dawn_native
+MACH_EXPORT const DawnProcTable* machDawnNativeGetProcs();
+
+
+// utils
+#include <GLFW/glfw3.h>
+
+typedef struct MachUtilsBackendBindingImpl* MachUtilsBackendBinding;
+MACH_EXPORT MachUtilsBackendBinding machUtilsCreateBinding(WGPUBackendType backendType, GLFWwindow* window, WGPUDevice device);
+MACH_EXPORT uint64_t machUtilsBackendBinding_getSwapChainImplementation(MachUtilsBackendBinding binding);
+MACH_EXPORT WGPUTextureFormat machUtilsBackendBinding_getPreferredSwapChainTextureFormat(MachUtilsBackendBinding binding);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif  // MACH_DAWNNATIVE_C_H_
