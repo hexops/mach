fce0b6fc409e62b84f531b493e310c3cd1c9175f
Ali Chraghi <63465728+AliChraghi@users.noreply.github.com>
2021-12-21 21:19:01 +0330

    update `update-upstream.sh`

--- glfw/update-upstream.sh
+++ glfw/update-upstream.sh
@@ -5,9 +5,8 @@ set -ex
 rm -rf upstream/
 mkdir upstream/
 cd upstream/
-git clone https://github.com/glfw/glfw
+git clone --depth 1 --branch 3.3.4 https://github.com/glfw/glfw
 cd glfw/
-git checkout 3.3.4
 
 # Remove non-C files
 rm -rf .appveyor.yml .git .gitattributes .gitignore .mailmap .travis.yml
