ba98560d7d09b751b5512337bd122712ce8371c8
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2021-12-24 01:05:47 -0700

    glfw: update sdk-linux-x86_64 to include Wayland protocol sources
    See https://github.com/hexops/sdk-linux-x86_64/pull/2

--- glfw/system_sdk.zig
+++ glfw/system_sdk.zig
@@ -27,7 +27,7 @@
 //! Best way to get this file in your own repository? We suggest just copying it, or importing it
 //! from a project that includes it if you're using one (e.g. mach-glfw)
 //!
-//! version: 1.0
+//! version: Dec 24, 2021
 
 const std = @import("std");
 const Builder = std.build.Builder;
@@ -46,7 +46,7 @@ pub const Options = struct {
 
     /// The Linux x86-64 SDK repository name.
     linux_x86_64: []const u8 = "sdk-linux-x86_64",
-    linux_x86_64_revision: []const u8 = "ce9ef1e0b45d982a399fac6a9456a47a5e1f0d2e",
+    linux_x86_64_revision: []const u8 = "ab7fa8f3a05b06e0b06f4277b484e27004bfb20f",
 
     /// If true, the Builder.sysroot will set to the SDK path. This has the drawback of preventing
     /// you from including headers, libraries, etc. from outside the SDK generally. However, it can
