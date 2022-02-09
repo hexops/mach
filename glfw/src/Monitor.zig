hexops/mach

commit 3e79a12f3d686b67b7962374c7fe5c8f98ff714c

glfw: dont call `getError` unless we need to

glfw/src/Window.zig
@@ -424,13 +424,13 @@ pub inline fn create(
     if (!ignore_hints_struct) hints.set();
     defer if (!ignore_hints_struct) defaultHints();
 
-    const handle = c.glfwCreateWindow(
+    if (c.glfwCreateWindow(
         @intCast(c_int, width),
         @intCast(c_int, height),
         &title[0],
         if (monitor) |m| m.handle else null,
         if (share) |w| w.handle else null,
-    );
+    )) |handle| return from(handle);
 
     getError() catch |err| return switch (err) {
         Error.NotInitialized => unreachable,
@@ -444,7 +444,7 @@ pub inline fn create(
         else => unreachable,
     };
 
-    return from(handle.?);
+    unreachable;
 }
 
 var testing_ignore_window_hints_struct = if (@import("builtin").is_test) false else @as(void, {});
@@ -1179,12 +1179,11 @@ pub inline fn swapBuffers(self: Window) error{ NoWindowContext, PlatformError }!
 /// see also: window_monitor, glfw.Window.setMonitor
 pub inline fn getMonitor(self: Window) ?Monitor {
     internal_debug.assertInitialized();
-    const monitor = c.glfwGetWindowMonitor(self.handle);
+    if (c.glfwGetWindowMonitor(self.handle)) |monitor| return Monitor{ .handle = monitor };
     getError() catch |err| return switch (err) {
         Error.NotInitialized => unreachable,
         else => unreachable,
     };
-    if (monitor) |m| return Monitor{ .handle = m };
     return null;
 }
 
@@ -1299,13 +1298,14 @@ pub const Attrib = enum(c_int) {
 pub inline fn getAttrib(self: Window, attrib: Attrib) error{PlatformError}!i32 {
     internal_debug.assertInitialized();
     const v = c.glfwGetWindowAttrib(self.handle, @enumToInt(attrib));
+    if (v != 0) return v;
     getError() catch |err| return switch (err) {
         Error.NotInitialized => unreachable,
         Error.InvalidEnum => unreachable,
         Error.PlatformError => @errSetCast(error{PlatformError}, err),
         else => unreachable,
     };
-    return v;
+    unreachable;
 }
 
 /// Sets an attribute of the specified window.

glfw/src/time.zig
@@ -30,11 +30,12 @@ const internal_debug = @import("internal_debug.zig");
 pub inline fn getTime() f64 {
     internal_debug.assertInitialized();
     const time = c.glfwGetTime();
+    if (time != 0) return time;
     getError() catch |err| return switch (err) {
         Error.NotInitialized => unreachable,
         else => unreachable,
     };
-    return time;
+    unreachable;
 }
 
 /// Sets the GLFW time.
@@ -88,11 +89,12 @@ pub inline fn setTime(time: f64) void {
 pub inline fn getTimerValue() u64 {
     internal_debug.assertInitialized();
     const value = c.glfwGetTimerValue();
+    if (value != 0) return value;
     getError() catch |err| return switch (err) {
         Error.NotInitialized => unreachable,
         else => unreachable,
     };
-    return value;
+    unreachable;
 }
 
 /// Returns the frequency, in Hz, of the raw timer.
@@ -107,11 +109,12 @@ pub inline fn getTimerValue() u64 {
 pub inline fn getTimerFrequency() u64 {
     internal_debug.assertInitialized();
     const frequency = c.glfwGetTimerFrequency();
+    if (frequency != 0) frequency;
     getError() catch |err| return switch (err) {
         Error.NotInitialized => unreachable,
         else => unreachable,
     };
-    return frequency;
+    unreachable;
 }
 
 test "getTime" {

glfw/src/Cursor.zig
@@ -63,13 +63,13 @@ pub const Shape = enum(i32) {
 pub inline fn create(image: Image, xhot: i32, yhot: i32) error{PlatformError}!Cursor {
     internal_debug.assertInitialized();
     const img = image.toC();
-    const cursor = c.glfwCreateCursor(&img, @intCast(c_int, xhot), @intCast(c_int, yhot));
+    if (c.glfwCreateCursor(&img, @intCast(c_int, xhot), @intCast(c_int, yhot))) |cursor| return Cursor{ .ptr = cursor };
     getError() catch |err| return switch (err) {
         Error.NotInitialized => unreachable,
         Error.PlatformError => @errSetCast(error{PlatformError}, err),
         else => unreachable,
     };
-    return Cursor{ .ptr = cursor.? };
+    unreachable;
 }
 
 /// Creates a cursor with a standard shape.
@@ -83,14 +83,14 @@ pub inline fn create(image: Image, xhot: i32, yhot: i32) error{PlatformError}!Cu
 /// see also: cursor_object, glfwCreateCursor
 pub inline fn createStandard(shape: Shape) error{PlatformError}!Cursor {
     internal_debug.assertInitialized();
-    const cursor = c.glfwCreateStandardCursor(@intCast(c_int, @enumToInt(shape)));
+    if (c.glfwCreateStandardCursor(@intCast(c_int, @enumToInt(shape)))) |cursor| return Cursor{ .ptr = cursor };
     getError() catch |err| return switch (err) {
         Error.NotInitialized => unreachable,
         Error.InvalidEnum => unreachable,
         Error.PlatformError => @errSetCast(error{PlatformError}, err),
         else => unreachable,
     };
-    return Cursor{ .ptr = cursor.? };
+    unreachable;
 }
 
 /// Destroys a cursor.

glfw/src/Joystick.zig
@@ -414,7 +414,7 @@ pub inline fn setCallback(callback: ?fn (joystick: Joystick, event: Event) void)
 /// @ingroup input
 pub inline fn updateGamepadMappings(gamepad_mappings: [*:0]const u8) error{InvalidValue}!void {
     internal_debug.assertInitialized();
-    _ = c.glfwUpdateGamepadMappings(gamepad_mappings);
+    if (c.glfwUpdateGamepadMappings(gamepad_mappings) == c.GLFW_TRUE) return;
     getError() catch |err| return switch (err) {
         Error.NotInitialized => unreachable,
         // TODO: Maybe return as 'ParseError' here?

glfw/src/clipboard.zig
@@ -48,7 +48,7 @@ pub inline fn setClipboardString(value: [*:0]const u8) error{PlatformError}!void
 /// see also: clipboard, glfwSetClipboardString
 pub inline fn getClipboardString() error{ FormatUnavailable, PlatformError }![:0]const u8 {
     internal_debug.assertInitialized();
-    const value = c.glfwGetClipboardString(null);
+    if (c.glfwGetClipboardString(null)) |c_str| return std.mem.span(c_str);
     getError() catch |err| return switch (err) {
         Error.NotInitialized => unreachable,
         Error.FormatUnavailable,
@@ -56,7 +56,7 @@ pub inline fn getClipboardString() error{ FormatUnavailable, PlatformError }![:0
         => @errSetCast(error{ FormatUnavailable, PlatformError }, err),
         else => unreachable,
     };
-    return std.mem.span(value);
+    unreachable;
 }
 
 test "setClipboardString" {

glfw/src/key.zig
@@ -243,13 +243,14 @@ pub const Key = enum(c_int) {
     pub inline fn getScancode(self: Key) error{PlatformError}!i32 {
         internal_debug.assertInitialized();
         const scancode = cc.glfwGetKeyScancode(@enumToInt(self));
+        if (scancode != -1) return scancode;
         getError() catch |err| return switch (err) {
             Error.NotInitialized => unreachable,
             Error.InvalidEnum => unreachable,
             Error.PlatformError => @errSetCast(error{PlatformError}, err),
             else => unreachable,
         };
-        return scancode;
+        unreachable;
     }
 };
 

glfw/src/main.zig
@@ -74,7 +74,7 @@ pub inline fn init(hints: InitHints) error{PlatformError}!void {
         initHint(init_hint, init_value);
     }
 
-    _ = c.glfwInit();
+    if (c.glfwInit() == c.GLFW_TRUE) return;
     getError() catch |err| return switch (err) {
         Error.PlatformError => @errSetCast(error{PlatformError}, err),
         else => unreachable,
@@ -210,11 +210,7 @@ fn initHint(hint: InitHint, value: anytype) void {
 ///
 /// @thread_safety This function may be called from any thread.
 pub inline fn getVersionString() [:0]const u8 {
-    const result = std.mem.span(c.glfwGetVersionString());
-    getError() catch |err| switch (err) {
-        else => unreachable,
-    };
-    return result;
+    return std.mem.span(c.glfwGetVersionString());
 }
 
 /// Processes all pending events.

commit 0cdac6c68a45f529964e6c120cdff33ea8e1260f

Fix minor regressions introduced in 3e79a12
3e79a12f3d686b67b7962374c7fe5c8f98ff714c

Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

glfw/src/Monitor.zig
@@ -394,7 +394,7 @@ pub inline fn getAll(allocator: mem.Allocator) mem.Allocator.Error![]Monitor {
         Error.NotInitialized => unreachable,
         else => unreachable,
     };
-    return &.{};
+    unreachable;
 }
 
 /// Returns the primary monitor.

glfw/src/Window.zig
@@ -1304,7 +1304,7 @@ pub inline fn getAttrib(self: Window, attrib: Attrib) error{PlatformError}!i32 {
         Error.PlatformError => |e| e,
         else => unreachable,
     };
-    unreachable;
+    return v;
 }
 
 /// Sets an attribute of the specified window.

glfw/src/time.zig
@@ -109,7 +109,7 @@ pub inline fn getTimerValue() u64 {
 pub inline fn getTimerFrequency() u64 {
     internal_debug.assertInitialized();
     const frequency = c.glfwGetTimerFrequency();
-    if (frequency != 0) frequency;
+    if (frequency != 0) return frequency;
     getError() catch |err| return switch (err) {
         Error.NotInitialized => unreachable,
         else => unreachable,