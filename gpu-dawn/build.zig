41c8f46ee8e5d75722bd19dea7f2107e31c32679
Release automation <stephen@hexops.com>
2022-02-13 04:45:36 +0000

    gpu-dawn: update to latest binary release

--- gpu-dawn/build.zig
+++ gpu-dawn/build.zig
@@ -83,7 +83,7 @@ pub const Options = struct {
     from_source: bool = false,
 
     /// The binary release version to use from https://github.com/hexops/mach-gpu-dawn/releases
-    binary_version: []const u8 = "release-d99a908",
+    binary_version: []const u8 = "release-59da7c7",
 
     /// Detects the default options to use for the given target.
     pub fn detectDefaults(self: Options, target: std.Target) Options {
