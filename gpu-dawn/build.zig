601a5a463530cae9da467740234341082b46844b
Stephen Gutekanst <stephen@hexops.com>
2022-02-12 23:02:12 -0700

    gpu-dawn: use linux-x86_64 binary releases
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu-dawn/build.zig
+++ gpu-dawn/build.zig
@@ -194,7 +194,7 @@ pub fn linkFromBinary(b: *Builder, step: *std.build.LibExeObjStep, options: Opti
     const triple = blk: {
         if (target.cpu.arch.isX86()) switch (target.os.tag) {
             .windows => return linkFromSource(b, step, options), // break :blk "windows-x86_64",
-            .linux => return linkFromSource(b, step, options), // break :blk "linux-x86_64",
+            .linux => break :blk "linux-x86_64",
             .macos => break :blk "macos-x86_64",
             else => return linkFromSource(b, step, options),
         };
