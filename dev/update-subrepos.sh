e276648c3f0ecdf193fabc50142997eb9128c97e
Stephen Gutekanst <stephen@hexops.com>
2022-02-12 17:24:48 -0700

    dev: use HTTPS subrepo remotes in CI
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- dev/update-subrepos.sh
+++ dev/update-subrepos.sh
@@ -2,8 +2,13 @@
 set -exuo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
-git remote add -f mach-glfw git@github.com:hexops/mach-glfw || true
-git fetch mach-glfw
+if [[ -z "${GITHUB_ACTIONS}" ]]; then
+    git remote add -f mach-glfw git@github.com:hexops/mach-glfw || true
+    git remote add -f mach-gpu-dawn git@github.com:hexops/mach-gpu-dawn || true
+else
+    git remote add -f mach-glfw https://github.com/hexops/mach-glfw || true
+    git remote add -f mach-gpu-dawn https://github.com/hexops/mach-gpu-dawn || true
+fi
 
-git remote add -f mach-gpu-dawn git@github.com:hexops/mach-gpu-dawn || true
+git fetch mach-glfw
 git fetch mach-gpu-dawn
