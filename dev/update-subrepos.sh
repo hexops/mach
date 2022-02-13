bd220b275cb8383f6c2d1231180525ad5f5a71aa
Stephen Gutekanst <stephen@hexops.com>
2022-02-12 18:15:29 -0700

    dev: correct bash conditional for CI auth
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- dev/update-subrepos.sh
+++ dev/update-subrepos.sh
@@ -2,12 +2,12 @@
 set -exuo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
-if [[ -z ${GITHUB_ACTIONS+""} ]]; then
-    git remote add -f mach-glfw git@github.com:hexops/mach-glfw || true
-    git remote add -f mach-gpu-dawn git@github.com:hexops/mach-gpu-dawn || true
-else
+if [ -n "$GITHUB_ACTIONS" ]; then
     git remote add -f mach-glfw "https://slimsag:$ACCESS_TOKEN@github.com/hexops/mach-glfw" || true
     git remote add -f mach-gpu-dawn "https://slimsag:$ACCESS_TOKEN@github.com/hexops/mach-gpu-dawn" || true
+else
+    git remote add -f mach-glfw git@github.com:hexops/mach-glfw || true
+    git remote add -f mach-gpu-dawn git@github.com:hexops/mach-gpu-dawn || true
 fi
 
 git fetch mach-glfw
