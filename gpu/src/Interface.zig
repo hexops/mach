42ac63ca02ef3b472d6389d598d0c8ee59462ed4
Stephen Gutekanst <stephen@hexops.com>
2022-03-06 20:28:10 -0700

    gpu: prepare to implement Surface
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/Interface.zig
+++ gpu/src/Interface.zig
@@ -8,5 +8,10 @@ vtable: *const VTable,
 
 pub const VTable = struct {
     // TODO(gpu): make these *const fn once stage2 is released.
-    free: fn (ptr: *anyopaque, buf: []u8, buf_align: u29, ret_addr: usize) void,
+    deinit: fn (ptr: *anyopaque) void,
 };
+
+// TODO:
+// WGPU_EXPORT void wgpuInstanceRequestAdapter(WGPUInstance instance, WGPURequestAdapterOptions const * options, WGPURequestAdapterCallback callback, void * userdata);
+// WGPU_EXPORT void wgpuInstanceReference(WGPUInstance instance);
+// WGPU_EXPORT void wgpuInstanceRelease(WGPUInstance instance);
--- gpu/src/NativeInstance.zig
+++ gpu/src/NativeInstance.zig
@@ -1,18 +1,29 @@
 //! A native webgpu.h implementation of the gpu.Interface
 const c = @import("c.zig").c;
 const Interface = @import("Interface.zig");
+const Surface = @import("Surface.zig");
 
 const NativeInstance = @This();
 
 /// The WGPUInstance that is wrapped by this native instance.
 instance: c.WGPUInstance,
 
-/// Returns the gpu.Interface for interacting with this native instance.
-pub fn interface(native: NativeInstance) Interface {
-    _ = native;
-    @panic("not implemented");
+vtable: Interface.VTable,
 
+/// Wraps a native WGPUInstance to provide an implementation of the gpu.Interface.
+pub fn wrap(instance: c.WGPUInstance) NativeInstance {
+    return .{ .instance = instance };
+}
+
+/// Returns the gpu.Interface for interacting with this native instance.
+pub fn interface(native: *const NativeInstance) Interface {
+    return .{
+        .ptr = native,
+        .vtable = native.vtable,
+    };
     // TODO: implement Interface
+    // WGPU_EXPORT void wgpuInstanceReference(WGPUInstance instance);
+    // WGPU_EXPORT void wgpuInstanceRelease(WGPUInstance instance);
 
     // TODO: implement Device interface
 
@@ -25,7 +36,9 @@ pub fn interface(native: NativeInstance) Interface {
     // WGPU_EXPORT void wgpuAdapterGetProperties(WGPUAdapter adapter, WGPUAdapterProperties * properties);
 }
 
-/// Wraps a native WGPUInstance to provide an implementation of the gpu.Interface.
-pub fn wrap(instance: c.WGPUInstance) NativeInstance {
-    return .{ .instance = instance };
+pub fn createSurface(native: *const NativeInstance, descriptor: *const Surface.Descriptor) Surface {
+    _ = native;
+    _ = descriptor;
+    // TODO:
+    // WGPU_EXPORT WGPUSurface wgpuInstanceCreateSurface(WGPUInstance instance, WGPUSurfaceDescriptor const * descriptor);
 }
