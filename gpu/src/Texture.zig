1563fc7246f64144d5988b8926106d0e4104c505
Stephen Gutekanst <stephen@hexops.com>
2022-03-09 11:48:39 -0700

    gpu: implement Texture
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

---
+++ gpu/src/Texture.zig
@@ -0,0 +1,23 @@
+const Texture = @This();
+
+/// The type erased pointer to the Texture implementation
+/// Equal to c.WGPUTexture for NativeInstance.
+ptr: *anyopaque,
+vtable: *const VTable,
+
+pub const VTable = struct {
+    reference: fn (ptr: *anyopaque) void,
+    release: fn (ptr: *anyopaque) void,
+    // TODO:
+    // WGPU_EXPORT WGPUTextureView wgpuTextureCreateView(WGPUTexture texture, WGPUTextureViewDescriptor const * descriptor);
+    // WGPU_EXPORT void wgpuTextureDestroy(WGPUTexture texture);
+    // WGPU_EXPORT void wgpuTextureSetLabel(WGPUTexture texture, char const * label);
+};
+
+pub inline fn reference(texture: Texture) void {
+    texture.vtable.reference(texture.ptr);
+}
+
+pub inline fn release(texture: Texture) void {
+    texture.vtable.release(texture.ptr);
+}
