c8c2ac0e4e3dc89498d9a9f39df0a3d65e5d743e
Stephen Gutekanst <stephen@hexops.com>
2022-03-11 15:15:17 -0700

    gpu: add CompilationMessage
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/structs.zig
+++ gpu/src/structs.zig
@@ -3,6 +3,7 @@ const Buffer = @import("Buffer.zig");
 const Sampler = @import("Sampler.zig");
 const TextureView = @import("TextureView.zig");
 const BufferBindingType = @import("enums.zig").BufferBindingType;
+const CompilationMessageType = @import("enums.zig").CompilationMessageType;
 
 pub const BindGroupEntry = struct {
     binding: u32,
@@ -17,9 +18,19 @@ pub const BufferBindingLayout = struct {
     type: BufferBindingType,
     has_dynamic_offset: bool,
     min_binding_size: u64,
-}
+};
+
+pub const CompilationMessage = struct {
+    message: [:0]const u8,
+    type: CompilationMessageType,
+    line_num: u64,
+    line_pos: u64,
+    offset: u64,
+    length: u64,
+};
 
 test "syntax" {
     _ = BindGroupEntry;
     _ = BufferBindingLayout;
+    _ = CompilationMessage;
 }
