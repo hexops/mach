8a8fbc9a745197720fe9ab2c567724873405522f
Stephen Gutekanst <stephen@hexops.com>
2022-03-11 16:01:45 -0700

    gpu: move TextureViewDimension -> Texture.ViewDimension
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/Texture.zig
+++ gpu/src/Texture.zig
@@ -170,6 +170,16 @@ pub const SampleType = enum(u32) {
     uint = 0x00000005,
 };
 
+pub const ViewDimension = enum(u32) {
+    dimension_none = 0x00000000,
+    dimension_1d = 0x00000001,
+    dimension_2d = 0x00000002,
+    dimension_2d_array = 0x00000003,
+    dimension_cube = 0x00000004,
+    dimension_cube_array = 0x00000005,
+    dimension_3d = 0x00000006,
+};
+
 test "syntax" {
     _ = VTable;
     _ = reference;
@@ -181,4 +191,5 @@ test "syntax" {
     _ = ComponentType;
     _ = Dimension;
     _ = SampleType;
+    _ = ViewDimension;
 }
