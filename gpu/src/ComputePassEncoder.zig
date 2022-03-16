2981e5e352782209f732a6fc696f20e8c684b981
Stephen Gutekanst <stephen@hexops.com>
2022-03-16 11:50:34 -0700

    gpu: begin translating ComputePassEncoder methods
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/ComputePassEncoder.zig
+++ gpu/src/ComputePassEncoder.zig
@@ -10,17 +10,24 @@ vtable: *const VTable,
 pub const VTable = struct {
     reference: fn (ptr: *anyopaque) void,
     release: fn (ptr: *anyopaque) void,
-    // TODO:
+    // dispatch: fn (ptr: *anyopaque, workgroup_count_x: u32, workgroup_count_y: u32, workgroup_count_z: u32) void,
     // WGPU_EXPORT void wgpuComputePassEncoderDispatch(WGPUComputePassEncoder computePassEncoder, uint32_t workgroupCountX, uint32_t workgroupCountY, uint32_t workgroupCountZ);
+    // dispatchIndirect: fn (ptr: *anyopaque, indirect_buffer: Buffer, indirect_offset: u64) void,
     // WGPU_EXPORT void wgpuComputePassEncoderDispatchIndirect(WGPUComputePassEncoder computePassEncoder, WGPUBuffer indirectBuffer, uint64_t indirectOffset);
+    // end: fn (ptr: *anyopaque) void,
     // WGPU_EXPORT void wgpuComputePassEncoderEnd(WGPUComputePassEncoder computePassEncoder);
-    // WGPU_EXPORT void wgpuComputePassEncoderEndPass(WGPUComputePassEncoder computePassEncoder);
+    // insertDebugMarker: fn (ptr: *anyopaque, marker_label: [*:0]const u8) void,
     // WGPU_EXPORT void wgpuComputePassEncoderInsertDebugMarker(WGPUComputePassEncoder computePassEncoder, char const * markerLabel);
+    // popDebugGroup: fn (ptr: *anyopaque) void,
     // WGPU_EXPORT void wgpuComputePassEncoderPopDebugGroup(WGPUComputePassEncoder computePassEncoder);
+    // pushDebugGroup: fn (ptr: *anyopaque, group_label: [*:0]const u8) void,
     // WGPU_EXPORT void wgpuComputePassEncoderPushDebugGroup(WGPUComputePassEncoder computePassEncoder, char const * groupLabel);
+    // setBindGroup: fn (ptr: *anyopaque, group_index: u32, group: BindGroup, dynamic_offsets: []u32) void,
     // WGPU_EXPORT void wgpuComputePassEncoderSetBindGroup(WGPUComputePassEncoder computePassEncoder, uint32_t groupIndex, WGPUBindGroup group, uint32_t dynamicOffsetCount, uint32_t const * dynamicOffsets);
     setLabel: fn (ptr: *anyopaque, label: [:0]const u8) void,
+    // setPipeline: fn (ptr: *anyopaque, pipeline: ComputePipeline) void,
     // WGPU_EXPORT void wgpuComputePassEncoderSetPipeline(WGPUComputePassEncoder computePassEncoder, WGPUComputePipeline pipeline);
+    // writeTimestamp: fn (ptr: *anyopaque, query_set: QuerySet, query_index: u32) void,
     // WGPU_EXPORT void wgpuComputePassEncoderWriteTimestamp(WGPUComputePassEncoder computePassEncoder, WGPUQuerySet querySet, uint32_t queryIndex);
 };
 
