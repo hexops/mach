1277ffccb00cd035ab15da2ba32b21155f29958f
Stephen Gutekanst <stephen@hexops.com>
2022-03-17 10:37:29 -0700

    gpu: implement ComputePassEncoder.dispatchIndirect
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/ComputePassEncoder.zig
+++ gpu/src/ComputePassEncoder.zig
@@ -2,6 +2,7 @@ const ComputePassTimestampWrite = @import("structs.zig").ComputePassTimestampWri
 const ComputePipeline = @import("ComputePipeline.zig");
 const QuerySet = @import("QuerySet.zig");
 const BindGroup = @import("BindGroup.zig");
+const Buffer = @import("Buffer.zig");
 
 const ComputePassEncoder = @This();
 
@@ -14,8 +15,7 @@ pub const VTable = struct {
     reference: fn (ptr: *anyopaque) void,
     release: fn (ptr: *anyopaque) void,
     dispatch: fn (ptr: *anyopaque, workgroup_count_x: u32, workgroup_count_y: u32, workgroup_count_z: u32) void,
-    // dispatchIndirect: fn (ptr: *anyopaque, indirect_buffer: Buffer, indirect_offset: u64) void,
-    // WGPU_EXPORT void wgpuComputePassEncoderDispatchIndirect(WGPUComputePassEncoder computePassEncoder, WGPUBuffer indirectBuffer, uint64_t indirectOffset);
+    dispatchIndirect: fn (ptr: *anyopaque, indirect_buffer: Buffer, indirect_offset: u64) void,
     end: fn (ptr: *anyopaque) void,
     insertDebugMarker: fn (ptr: *anyopaque, marker_label: [*:0]const u8) void,
     popDebugGroup: fn (ptr: *anyopaque) void,
@@ -41,6 +41,14 @@ pub inline fn dispatch(
     workgroup_count_z: u32,
 ) void {
     enc.vtable.dispatch(enc.ptr, workgroup_count_x, workgroup_count_y, workgroup_count_z);
+}
+
+pub inline fn dispatchIndirect(
+    enc: ComputePassEncoder,
+    indirect_buffer: Buffer,
+    indirect_offset: u64,
+) void {
+    enc.vtable.dispatchIndirect(enc.ptr, indirect_buffer, indirect_offset);
 }
 
 pub inline fn end(enc: ComputePassEncoder) void {
@@ -90,6 +98,7 @@ test {
     _ = reference;
     _ = release;
     _ = dispatch;
+    _ = dispatchIndirect;
     _ = end;
     _ = insertDebugMarker;
     _ = popDebugGroup;
