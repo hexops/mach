13de9d6a8cf0b7e5094186f2ddb5867075b747de
Stephen Gutekanst <stephen@hexops.com>
2022-03-31 16:44:33 +0000

    gpu: make -Ddawn-from-source=true work in gpu/ example
    Previously this was only working in the gpu-dawn/ example.
    
    Fixes hexops/mach#196
    
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/build.zig
+++ gpu/build.zig
@@ -6,18 +6,22 @@ pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
     const target = b.standardTargetOptions(.{});
 
+    const gpu_dawn_options = gpu_dawn.Options{
+        .from_source = b.option(bool, "dawn-from-source", "Build Dawn from source") orelse false,
+    };
+
     const lib = b.addStaticLibrary("gpu", "src/main.zig");
     lib.setTarget(target);
     lib.setBuildMode(mode);
     lib.install();
     glfw.link(b, lib, .{});
-    gpu_dawn.link(b, lib, .{});
+    gpu_dawn.link(b, lib, gpu_dawn_options);
 
     const main_tests = b.addTest("src/main.zig");
     main_tests.setTarget(target);
     main_tests.setBuildMode(mode);
     glfw.link(b, main_tests, .{});
-    gpu_dawn.link(b, main_tests, .{});
+    gpu_dawn.link(b, main_tests, gpu_dawn_options);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
@@ -30,7 +34,7 @@ pub fn build(b: *std.build.Builder) void {
     example.addPackagePath("gpu", "src/main.zig");
     example.addPackagePath("glfw", "libs/mach-glfw/src/main.zig");
     glfw.link(b, example, .{});
-    gpu_dawn.link(b, example, .{});
+    gpu_dawn.link(b, example, gpu_dawn_options);
 
     const example_run_cmd = example.run();
     example_run_cmd.step.dependOn(b.getInstallStep());
