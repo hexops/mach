657091ed65f715c05262ebf8b413ddc1a0d36891
iddev5 <ayushbardhan5@gmail.com>
2022-05-15 12:33:50 +0530

    mach: Reorganised native backend files, moved structs and fixed circular dependency

--- src/structs.zig
+++ src/structs.zig
@@ -1,3 +1,5 @@
+const gpu = @import("gpu");
+
 pub const Size = struct {
     width: u32,
     height: u32,
@@ -6,4 +8,46 @@ pub const Size = struct {
 pub const SizeOptional = struct {
     width: ?u32,
     height: ?u32,
+};
+
+pub const VSyncMode = enum {
+    /// Potential screen tearing.
+    /// No synchronization with monitor, render frames as fast as possible.
+    none,
+
+    /// No tearing, synchronizes rendering with monitor refresh rate, rendering frames when ready.
+    ///
+    /// Tries to stay one frame ahead of the monitor, so when it's ready for the next frame it is
+    /// already prepared.
+    double,
+
+    /// No tearing, synchronizes rendering with monitor refresh rate, rendering frames when ready.
+    ///
+    /// Tries to stay two frames ahead of the monitor, so when it's ready for the next frame it is
+    /// already prepared.
+    triple,
+};
+
+/// Application options that can be configured at init time.
+pub const Options = struct {
+    /// The title of the window.
+    title: [*:0]const u8 = "Mach engine",
+
+    /// The width of the window.
+    width: u32 = 640,
+
+    /// The height of the window.
+    height: u32 = 480,
+
+    /// Monitor synchronization modes.
+    vsync: VSyncMode = .double,
+
+    /// GPU features required by the application.
+    required_features: ?[]gpu.Feature = null,
+
+    /// GPU limits required by the application.
+    required_limits: ?gpu.Limits = null,
+
+    /// Whether the application has a preference for low power or high performance GPU.
+    power_preference: gpu.PowerPreference = .none,
 };
