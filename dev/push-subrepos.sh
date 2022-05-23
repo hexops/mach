26cd36e6ca432b894e8871ea73788116a00a647e
Stephen Gutekanst <stephen@hexops.com>
2022-05-22 23:38:02 -0700

    dev: add mach-freetype subrepo
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- dev/push-subrepos.sh
+++ dev/push-subrepos.sh
@@ -5,3 +5,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")"/..
 ./dev/update-subrepos.sh
 git subtree push --prefix glfw mach-glfw main
 git subtree push --prefix gpu-dawn mach-gpu-dawn main
+git subtree push --prefix freetype mach-freetype main
