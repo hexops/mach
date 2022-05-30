21c49ff9be2167a6f491535ac1a9c40daf456e3c
iddev5 <ayushbardhan5@gmail.com>
2022-05-30 13:31:22 +0530

    mach: Move all platform specific files to platform/ directory

--- src/platform.zig
+++ src/platform.zig
@@ -1,6 +1,6 @@
 const builtin = @import("builtin");
 
-const Platform = if (builtin.cpu.arch == .wasm32) @import("wasm.zig") else @import("native.zig");
+const Platform = if (builtin.cpu.arch == .wasm32) @import("platform/wasm.zig") else @import("platform/native.zig");
 
 // TODO: verify declarations and its signatures
 pub const CoreType = Platform.Core;
