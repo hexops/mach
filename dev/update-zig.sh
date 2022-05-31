2de8918e9a031adffaf1088cd39d0ba4fea02c54
Stephen Gutekanst <stephen@hexops.com>
2022-05-31 02:48:29 -0700

    dev: make update-zig.sh update README version
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- dev/update-zig.sh
+++ dev/update-zig.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
 update_zig() {
+    gsed -i 's|\(Currently tested with: \).*|\1'"$1"'|' $2
     gsed -i 's|\(https://ziglang.org/builds/zig-[^/ -]*-[^/ -]*-\)[^/ ]*\(\(\.tar\.xz\)[^/ ]*\)|\1'"$1"'\2|' $2
     gsed -i 's|\(https://ziglang.org/builds/zig-[^/ -]*-[^/ -]*-\)[^/ ]*\(\(\.zip\)[^/ ]*\)|\1'"$1"'\2|' $2
 }
@@ -10,10 +11,10 @@ update_zig() {
 if [ -n "${ZIG_VERSION:-}" ]; then
     version="${ZIG_VERSION:-}"
 
+    update_zig "$version" README.md
+
     sources=$(find . | grep './.github/workflows' | grep -v 'third_party/' | grep -v 'DirectXShaderCompiler' | grep -v '/libs/' | grep '\.yml')
     echo "$sources" | while read line ; do update_zig "$version" "$line" ; done
-
-    update_zig "$version" README.md
 else
     echo "must specify e.g. ZIG_VERSION=0.10.0-dev.2017+a0a2ce92c"
     exit 0
