2b7527b028725096eeb331f425f7322431cd53e2
Ali Chraghi <63465728+alichraghi@users.noreply.github.com>
2022-06-10 15:27:43 +0430

    freetype:harfbuzz: 100% blob and 50% buffer binding

---
+++ freetype/src/harfbuzz/c.zig
@@ -0,0 +1,3 @@
+pub usingnamespace @cImport({
+    @cInclude("hb.h");
+});
--- freetype/src/harfbuzz/main.zig
+++ freetype/src/harfbuzz/main.zig
@@ -1,1 +1,10 @@
-//
+pub usingnamespace @import("blob.zig");
+pub usingnamespace @import("buffer.zig");
+pub usingnamespace @import("common.zig");
+pub const c = @import("c.zig");
+
+const utils = @import("utils");
+
+test {
+    utils.refAllDecls(@This());
+}
