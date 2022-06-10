40d7bf6ba6961aa5b515fc1641de069fc60c3640
Stephen Gutekanst <stephen@hexops.com>
2022-06-09 23:00:05 -0700

    ecs: add pkg declaration, remove static library
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- ecs/build.zig
+++ ecs/build.zig
@@ -2,16 +2,17 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
-    const target = b.standardTargetOptions(.{});
-
-    const lib = b.addStaticLibrary("ecs", "src/main.zig");
-    lib.setBuildMode(mode);
-    lib.setTarget(target);
-    lib.install();
 
     const main_tests = b.addTest("src/main.zig");
+    main_tests.addPackage(pkg);
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
 }
+
+pub const pkg = std.build.Pkg{
+    .name = "ecs",
+    .source = .{ .path = "src/main.zig" },
+    .dependencies = &[_]std.build.Pkg{},
+};
