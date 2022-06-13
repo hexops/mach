d2996fcbabd2d932ee6e27ff4633bb9be384ffc2
Stephen Gutekanst <stephen@hexops.com>
2022-06-13 01:49:32 -0700

    add TODO about dependency loop
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- examples/ecs-app/physics2d.zig
+++ examples/ecs-app/physics2d.zig
@@ -9,6 +9,17 @@ pub const module = ecs.Module(.{
     },
     // TODO: there would be systems that we register here. Functions that iterate over entities
     // with physics2d components like `.velocity` and calculate physics updates for them!
+    .system = system,
 });
 
 pub const Vec2 = struct { x: f32, y: f32 };
+
+// TODO: there is a real problem here, we cannot access `modules`: dependency loop.
+// modules -> physics2d.module -> system -> modules
+fn system(engine: *ecs.World(modules)) !void {
+    _ = engine;
+
+    // A real system would query the ECS for entities with components. This is just an example.
+    const player = try engine.entities.new();
+    try engine.entities.setComponent(player, .physics2d, .location, .{ .x = 0, .y = 0 });
+}
