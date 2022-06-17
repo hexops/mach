0ab847c984b35fc356dd54b25302e0a45ace3198
Ali Chraghi <alichraghi@pm.me>
2022-06-17 16:40:11 +0430

    freetype/harfbuzz: shape and partial shape_plan binding

---
+++ freetype/src/harfbuzz/shape.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const c = @import("c.zig");
+
+pub const ListShapers = struct {
+    index: usize,
+    list: [*c][*c]const u8,
+
+    pub fn init() ListShapers {
+        return .{ .index = 0, .list = c.hb_shape_list_shapers() };
+    }
+
+    pub fn next(self: *ListShapers) ?[]const u8 {
+        self.index += 1;
+        return std.mem.span(
+            self.list[self.index - 1] orelse return null,
+        );
+    }
+};
