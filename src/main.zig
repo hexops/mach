2257f0d7ff1bf7601fa08adf2dd6da942ae51a70
iddev5 <ayushbardhan5@gmail.com>
2022-06-20 13:11:12 +0530

    mach: Implement basic resource loading system
    Curently no real management has been implemented. It just loads the
    resource from the list of paths present and provides the raw data from
    it.

--- src/main.zig
+++ src/main.zig
@@ -2,3 +2,4 @@ pub usingnamespace @import("structs.zig");
 pub usingnamespace @import("enums.zig");
 pub const Engine = @import("Engine.zig");
 pub const Timer = @import("Timer.zig");
+pub const ResourceManager = @import("resource/ResourceManager.zig");
