6ec27861b4df113b5392d03738612a3a1e2998bf
Stephen Gutekanst <stephen@hexops.com>
2022-07-04 22:17:58 -0700

    mach: initial support for high-level ECS applications
    Adds experimental support for high-level ECS-based applications
    following hexops/mach#349
    
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/main.zig
+++ src/main.zig
@@ -3,3 +3,9 @@ pub usingnamespace @import("enums.zig");
 pub const Core = @import("Core.zig");
 pub const Timer = @import("Timer.zig");
 pub const ResourceManager = @import("resource/ResourceManager.zig");
+pub const gpu = @import("gpu");
+pub const ecs = @import("ecs");
+
+// Engine exports
+pub const App = @import("engine.zig").App;
+pub const module = @import("engine.zig").module;
