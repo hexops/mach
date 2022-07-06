d10ab25c5d0c853d50d0e0c30b53709d19a7cf22
iddev5 <ayushbardhan5@gmail.com>
2022-07-06 17:49:35 +0530

    js-runtime: Upload source code from upstream

---
+++ js-runtime/build.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    const mode = b.standardReleaseOptions();
+
+    const main_tests = b.addTest("src/main.zig");
+    main_tests.addPackage(pkg);
+    main_tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&main_tests.step);
+}
+
+pub const pkg = std.build.Pkg{
+    .name = "js-runtime",
+    .source = .{ .path = thisDir() ++ "/src/main.zig" },
+    .dependencies = &[_]std.build.Pkg{},
+};
+
+fn thisDir() []const u8 {
+    return std.fs.path.dirname(@src().file) orelse ".";
+}
