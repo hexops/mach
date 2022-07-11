327b0f92578396e6069b4386921cad2a0196020e
Stephen Gutekanst <stephen@hexops.com>
2022-07-11 07:56:07 -0700

    gpu: add PresentMode enum
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/types.zig
+++ gpu/src/types.zig
@@ -171,6 +171,12 @@ pub const PowerPreference = enum(u32) {
     high_performance = 0x00000002,
 };
 
+pub const PresentMode = enum(u32) {
+    immediate = 0x00000000,
+    mailbox = 0x00000001,
+    fifo = 0x00000002,
+};
+
 test "BackendType name" {
     try testing.expectEqualStrings("Vulkan", BackendType.vulkan.name());
 }
