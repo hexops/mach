8ced6a43ad913907b31cf5648a97c79fc04f4fda
Stephen Gutekanst <stephen@hexops.com>
2022-07-11 08:13:23 -0700

    gpu: add Texture.ComponentType enum
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/Texture.zig
+++ gpu/src/Texture.zig
@@ -7,3 +7,10 @@ pub const Aspect = enum(u32) {
     plane0_only = 0x00000003,
     plane1_only = 0x00000004,
 };
+
+pub const ComponentType = enum(u32) {
+    float = 0x00000000,
+    sint = 0x00000001,
+    uint = 0x00000002,
+    depth_comparison = 0x00000003,
+};
