a0e8ef77fc76a62662a29d952d9a858766811f6d
Stephen Gutekanst <stephen@hexops.com>
2022-07-11 08:33:51 -0700

    gpu: add TextureView.Dimension enum
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/TextureView.zig
+++ gpu/src/TextureView.zig
@@ -1,1 +1,11 @@
 ptr: *anyopaque,
+
+pub const Dimension = enum(u32) {
+    dimension_undef = 0x00000000,
+    dimension_1d = 0x00000001,
+    dimension_2d = 0x00000002,
+    dimension_2d_array = 0x00000003,
+    dimension_cube = 0x00000004,
+    dimension_cube_array = 0x00000005,
+    dimension_3d = 0x00000006,
+};
