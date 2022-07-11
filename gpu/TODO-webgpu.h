e8be51eb52909f758cfefb58a3a942405c884164
Stephen Gutekanst <stephen@hexops.com>
2022-07-11 08:33:51 -0700

    gpu: add TextureView.Dimension enum
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/TODO-webgpu.h
+++ gpu/TODO-webgpu.h
@@ -1,15 +1,5 @@
 typedef uint32_t WGPUFlags;
 
-typedef enum WGPUTextureViewDimension {
-    WGPUTextureViewDimension_Undefined = 0x00000000,
-    WGPUTextureViewDimension_1D = 0x00000001,
-    WGPUTextureViewDimension_2D = 0x00000002,
-    WGPUTextureViewDimension_2DArray = 0x00000003,
-    WGPUTextureViewDimension_Cube = 0x00000004,
-    WGPUTextureViewDimension_CubeArray = 0x00000005,
-    WGPUTextureViewDimension_3D = 0x00000006,
-    WGPUTextureViewDimension_Force32 = 0x7FFFFFFF
-} WGPUTextureViewDimension;
 
 typedef enum WGPUVertexFormat {
     WGPUVertexFormat_Undefined = 0x00000000,
