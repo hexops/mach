6793e334f51910bff3314fa96efd827faba53fb4
Stephen Gutekanst <stephen@hexops.com>
2022-07-11 06:46:06 -0700

    gpu: add FeatureName enum
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/TODO-webgpu.h
+++ gpu/TODO-webgpu.h
@@ -1,24 +1,5 @@
 typedef uint32_t WGPUFlags;
 
-typedef enum WGPUFeatureName {
-    WGPUFeatureName_Undefined = 0x00000000,
-    WGPUFeatureName_DepthClipControl = 0x00000001,
-    WGPUFeatureName_Depth32FloatStencil8 = 0x00000002,
-    WGPUFeatureName_TimestampQuery = 0x00000003,
-    WGPUFeatureName_PipelineStatisticsQuery = 0x00000004,
-    WGPUFeatureName_TextureCompressionBC = 0x00000005,
-    WGPUFeatureName_TextureCompressionETC2 = 0x00000006,
-    WGPUFeatureName_TextureCompressionASTC = 0x00000007,
-    WGPUFeatureName_IndirectFirstInstance = 0x00000008,
-    WGPUFeatureName_DepthClamping = 0x000003E8,
-    WGPUFeatureName_DawnShaderFloat16 = 0x000003E9,
-    WGPUFeatureName_DawnInternalUsages = 0x000003EA,
-    WGPUFeatureName_DawnMultiPlanarFormats = 0x000003EB,
-    WGPUFeatureName_DawnNative = 0x000003EC,
-    WGPUFeatureName_ChromiumExperimentalDp4a = 0x000003ED,
-    WGPUFeatureName_Force32 = 0x7FFFFFFF
-} WGPUFeatureName;
-
 typedef enum WGPUFilterMode {
     WGPUFilterMode_Nearest = 0x00000000,
     WGPUFilterMode_Linear = 0x00000001,
