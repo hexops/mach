78685c19a040b14b589d35e8fa20ac980a458443
Stephen Gutekanst <stephen@hexops.com>
2022-07-10 21:31:10 -0700

    gpu: add BackendType enum
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/types.zig
+++ gpu/src/types.zig
@@ -2,3 +2,27 @@ pub const AlphaMode = enum(u32) {
     premultiplied = 0x00000000,
     unpremultiplied = 0x00000001,
 };
+
+pub const BackendType = enum(u32) {
+    nul,
+    webgpu,
+    d3d11,
+    d3d12,
+    metal,
+    vulkan,
+    opengl,
+    opengles,
+};
+
+pub fn backendTypeName(t: BackendType) []const u8 {
+    return switch (t) {
+        .nul => "Null",
+        .webgpu => "WebGPU",
+        .d3d11 => "D3D11",
+        .d3d12 => "D3D12",
+        .metal => "Metal",
+        .vulkan => "Vulkan",
+        .opengl => "OpenGL",
+        .opengles => "OpenGLES",
+    };
+}
