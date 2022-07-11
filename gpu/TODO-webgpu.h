71e6049ada689fb20a4eb224bfe2199be4a7220e
Stephen Gutekanst <stephen@hexops.com>
2022-07-11 07:53:09 -0700

    gpu: add IndexFormat enum
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/TODO-webgpu.h
+++ gpu/TODO-webgpu.h
@@ -1,12 +1,5 @@
 typedef uint32_t WGPUFlags;
 
-typedef enum WGPUIndexFormat {
-    WGPUIndexFormat_Undefined = 0x00000000,
-    WGPUIndexFormat_Uint16 = 0x00000001,
-    WGPUIndexFormat_Uint32 = 0x00000002,
-    WGPUIndexFormat_Force32 = 0x7FFFFFFF
-} WGPUIndexFormat;
-
 typedef enum WGPULoadOp {
     WGPULoadOp_Undefined = 0x00000000,
     WGPULoadOp_Clear = 0x00000001,
