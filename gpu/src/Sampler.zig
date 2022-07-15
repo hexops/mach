eed000ebd9890d9c317519cb6466e5d25b550c85
Stephen Gutekanst <stephen@hexops.com>
2022-07-15 00:22:34 -0700

    gpu: make Sampler an enum with methods
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/Sampler.zig
+++ gpu/src/Sampler.zig
@@ -1,14 +1,18 @@
-ptr: *anyopaque,
+pub const Sampler = enum(usize) {
+    _,
 
-pub const AddressMode = enum(u32) {
-    repeat = 0x00000000,
-    mirror_repeat = 0x00000001,
-    clamp_to_edge = 0x00000002,
-};
+    pub const none: Sampler = @intToEnum(Sampler, 0);
 
-pub const BindingType = enum(u32) {
-    undef = 0x00000000,
-    filtering = 0x00000001,
-    non_filtering = 0x00000002,
-    comparison = 0x00000003,
+    pub const AddressMode = enum(u32) {
+        repeat = 0x00000000,
+        mirror_repeat = 0x00000001,
+        clamp_to_edge = 0x00000002,
+    };
+
+    pub const BindingType = enum(u32) {
+        undef = 0x00000000,
+        filtering = 0x00000001,
+        non_filtering = 0x00000002,
+        comparison = 0x00000003,
+    };
 };
