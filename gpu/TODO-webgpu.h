c7798edafba4d037e316aedc276320d8d6bca694
Stephen Gutekanst <stephen@hexops.com>
2022-07-15 01:36:40 -0700

    gpu: add Limits
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/TODO-webgpu.h
+++ gpu/TODO-webgpu.h
@@ -84,37 +84,6 @@ typedef struct WGPUInstanceDescriptor {
     next_in_chain: *const ChainedStruct,
 } WGPUInstanceDescriptor;
 
-typedef struct WGPULimits {
-    uint32_t maxTextureDimension1D;
-    uint32_t maxTextureDimension2D;
-    uint32_t maxTextureDimension3D;
-    uint32_t maxTextureArrayLayers;
-    uint32_t maxBindGroups;
-    uint32_t maxDynamicUniformBuffersPerPipelineLayout;
-    uint32_t maxDynamicStorageBuffersPerPipelineLayout;
-    uint32_t maxSampledTexturesPerShaderStage;
-    uint32_t maxSamplersPerShaderStage;
-    uint32_t maxStorageBuffersPerShaderStage;
-    uint32_t maxStorageTexturesPerShaderStage;
-    uint32_t maxUniformBuffersPerShaderStage;
-    uint64_t maxUniformBufferBindingSize;
-    uint64_t maxStorageBufferBindingSize;
-    uint32_t minUniformBufferOffsetAlignment;
-    uint32_t minStorageBufferOffsetAlignment;
-    uint32_t maxVertexBuffers;
-    uint32_t maxVertexAttributes;
-    uint32_t maxVertexBufferArrayStride;
-    uint32_t maxInterStageShaderComponents;
-    uint32_t maxInterStageShaderVariables;
-    uint32_t maxColorAttachments;
-    uint32_t maxComputeWorkgroupStorageSize;
-    uint32_t maxComputeInvocationsPerWorkgroup;
-    uint32_t maxComputeWorkgroupSizeX;
-    uint32_t maxComputeWorkgroupSizeY;
-    uint32_t maxComputeWorkgroupSizeZ;
-    uint32_t maxComputeWorkgroupsPerDimension;
-} WGPULimits;
-
 typedef struct WGPUMultisampleState {
     next_in_chain: *const ChainedStruct,
     count: u32,
