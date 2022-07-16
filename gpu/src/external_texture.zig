128f87a3fdd447c64eb3920a9a0e70db899ea141
Stephen Gutekanst <stephen@hexops.com>
2022-07-16 00:02:20 -0700

    gpu: add ExternalTexture.Descriptor
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/external_texture.zig
+++ gpu/src/external_texture.zig
@@ -1,4 +1,5 @@
 pub const ChainedStruct = @import("types.zig").ChainedStruct;
+pub const TextureView = @import("texture_view.zig").TextureView;
 
 pub const ExternalTexture = enum(usize) {
     _,
@@ -13,5 +14,17 @@ pub const ExternalTexture = enum(usize) {
 
     pub const BindingLayout = extern struct {
         chain: ChainedStruct,
+    };
+
+    pub const Descriptor = extern struct {
+        next_in_chain: *const ChainedStruct,
+        label: ?[*:0]const u8 = null,
+        plane0: TextureView,
+        plane1: TextureView = TextureView.none, // nullable
+        do_yuv_to_rgb_conversion_only: bool,
+        yuv_to_rgb_conversion_matrix: ?[*]const f32 = null, // nullable
+        src_transform_function_parameters: [*]const f32,
+        dst_transform_function_parameters: [*]const f32,
+        gamut_conversion_matrix: [*]const f32,
     };
 };
