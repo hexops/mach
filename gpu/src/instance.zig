8f80cdc321ef785ffb91045928d0caa5722f30fc
Stephen Gutekanst <stephen@hexops.com>
2022-07-16 00:28:53 -0700

    gpu: add Instance.Descriptor
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/instance.zig
+++ gpu/src/instance.zig
@@ -1,6 +1,12 @@
+pub const ChainedStruct = @import("types.zig").ChainedStruct;
+
 pub const Instance = enum(usize) {
     _,
 
     // TODO: verify there is a use case for nullable value of this type.
     pub const none: Instance = @intToEnum(Instance, 0);
+
+    pub const Descriptor = extern struct {
+        next_in_chain: *const ChainedStruct,
+    };
 };
