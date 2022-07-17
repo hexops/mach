8320b4e1bc751553d529e736bdeb85ddc802050c
Stephen Gutekanst <stephen@hexops.com>
2022-07-16 19:57:25 -0700

    gpu: add RenderBundleEncoder.Descriptor
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/TODO-webgpu.h
+++ gpu/TODO-webgpu.h
@@ -1,14 +1,3 @@
-pub const WGPURenderBundleEncoderDescriptor = extern struct {
-    next_in_chain: *const ChainedStruct,
-    label: ?[*:0]const u8 = null,
-    color_formats_count: u32,
-    color_formats: [*]const TextureFormat,
-    depth_stencil_format: TextureFormat,
-    sample_count: u32,
-    depth_read_only: bool,
-    stencil_read_only: bool,
-};
-
 pub const WGPURenderPassDepthStencilAttachment = extern struct {
     view: TextureView,
     depth_load_op: LoadOp,
