5a6fd27ea6e834ed274292fd6007c6ab15fc94dd
Stephen Gutekanst <stephen@hexops.com>
2022-07-16 21:00:52 -0700

    gpu: add Device.Descriptor
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/device.zig
+++ gpu/src/device.zig
@@ -1,3 +1,8 @@
+const ChainedStruct = @import("types.zig").ChainedStruct;
+const FeatureName = @import("types.zig").FeatureName;
+const RequiredLimits = @import("types.zig").RequiredLimits;
+const Queue = @import("queue.zig").Queue;
+
 pub const Device = enum(usize) {
     _,
 
@@ -7,5 +12,14 @@ pub const Device = enum(usize) {
     pub const LostReason = enum(u32) {
         undef = 0x00000000,
         destroyed = 0x00000001,
+    };
+
+    pub const Descriptor = extern struct {
+        next_in_chain: *const ChainedStruct,
+        label: ?[*:0]const u8 = null,
+        required_features_count: u32,
+        required_features: [*]const FeatureName,
+        required_limits: ?*const RequiredLimits = null, // nullable
+        default_queue: Queue.Descriptor,
     };
 };
