0eff9e1cb334e7e25206dedf385422bdeddd40b6
Stephen Gutekanst <stephen@hexops.com>
2022-07-16 19:51:51 -0700

    gpu: add QuerySet.Descriptor
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/query_set.zig
+++ gpu/src/query_set.zig
@@ -1,6 +1,19 @@
+const ChainedStruct = @import("types.zig").ChainedStruct;
+const PipelineStatisticName = @import("types.zig").PipelineStatisticName;
+const QueryType = @import("types.zig").QueryType;
+
 pub const QuerySet = enum(usize) {
     _,
 
     // TODO: verify there is a use case for nullable value of this type.
     pub const none: QuerySet = @intToEnum(QuerySet, 0);
+
+    pub const Descriptor = extern struct {
+        next_in_chain: *const ChainedStruct,
+        label: ?[*:0]const u8 = null,
+        type: QueryType,
+        count: u32,
+        pipeline_statistics: [*]const PipelineStatisticName,
+        pipeline_statistics_count: u32,
+    };
 };
