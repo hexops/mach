10feadc33422d452157202fe6f599d8b3aff205c
Stephen Gutekanst <stephen@hexops.com>
2022-07-16 19:57:25 -0700

    gpu: add RenderBundleEncoder.Descriptor
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/render_bundle_encoder.zig
+++ gpu/src/render_bundle_encoder.zig
@@ -1,6 +1,20 @@
+const ChainedStruct = @import("types.zig").ChainedStruct;
+const Texture = @import("texture.zig").Texture;
+
 pub const RenderBundleEncoder = enum(usize) {
     _,
 
     // TODO: verify there is a use case for nullable value of this type.
     pub const none: RenderBundleEncoder = @intToEnum(RenderBundleEncoder, 0);
+
+    pub const Descriptor = extern struct {
+        next_in_chain: *const ChainedStruct,
+        label: ?[*:0]const u8 = null,
+        color_formats_count: u32,
+        color_formats: [*]const Texture.Format,
+        depth_stencil_format: Texture.Format,
+        sample_count: u32,
+        depth_read_only: bool,
+        stencil_read_only: bool,
+    };
 };
