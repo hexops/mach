2fba6b6240b7db625a9f7978047adacc59b0fe1f
Stephen Gutekanst <stephen@hexops.com>
2022-07-16 20:27:21 -0700

    gpu: add TextureView.Descriptor
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/texture_view.zig
+++ gpu/src/texture_view.zig
@@ -1,3 +1,6 @@
+const ChainedStruct = @import("types.zig").ChainedStruct;
+const Texture = @import("texture.zig").Texture;
+
 pub const TextureView = enum(usize) {
     _,
 
@@ -11,5 +14,17 @@ pub const TextureView = enum(usize) {
         dimension_cube = 0x00000004,
         dimension_cube_array = 0x00000005,
         dimension_3d = 0x00000006,
+    };
+
+    pub const Descriptor = extern struct {
+        next_in_chain: *const ChainedStruct,
+        label: ?[*:0]const u8 = null,
+        format: Texture.Format,
+        dimension: Dimension,
+        base_mip_level: u32,
+        mip_level_count: u32,
+        base_array_layer: u32,
+        array_layer_count: u32,
+        aspect: Texture.Aspect,
     };
 };
