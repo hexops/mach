9f41ea0b3acdfcd39d21cb906ebbbf624a5c485a
alichraghi <alichraghi@pm.me>
2022-07-22 15:24:58 +0430

    freetype: Bitmap Handling 100% Coverage

--- freetype/src/c.zig
+++ freetype/src/c.zig
@@ -1,6 +1,7 @@
 pub usingnamespace @cImport({
     @cInclude("hb-ft.h");
     @cInclude("freetype/ftbbox.h");
+    @cInclude("freetype/ftbitmap.h");
     @cInclude("freetype/ftcolor.h");
     @cInclude("freetype/ftlcdfil.h");
     @cInclude("freetype/ftsizes.h");
