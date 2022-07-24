8932f538419e6455f3e51d3b9a594f5bf8d86fc3
Stephen Gutekanst <stephen@hexops.com>
2022-07-24 14:54:20 -0700

    gpu: convert Queue from enum(usize) to *opaque
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/queue.zig
+++ gpu/src/queue.zig
@@ -1,20 +1,15 @@
 const ChainedStruct = @import("types.zig").ChainedStruct;
 
-pub const Queue = enum(usize) {
-    _,
+pub const Queue = *opaque {};
 
-    // TODO: verify there is a use case for nullable value of this type.
-    pub const none: Queue = @intToEnum(Queue, 0);
+pub const QueueWorkDoneStatus = enum(u32) {
+    success = 0x00000000,
+    err = 0x00000001,
+    unknown = 0x00000002,
+    device_lost = 0x00000003,
+};
 
-    pub const WorkDoneStatus = enum(u32) {
-        success = 0x00000000,
-        err = 0x00000001,
-        unknown = 0x00000002,
-        device_lost = 0x00000003,
-    };
-
-    pub const Descriptor = extern struct {
-        next_in_chain: *const ChainedStruct,
-        label: ?[*:0]const u8 = null,
-    };
+pub const QueueDescriptor = extern struct {
+    next_in_chain: *const ChainedStruct,
+    label: ?[*:0]const u8 = null,
 };
