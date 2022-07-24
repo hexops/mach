8719bab867b5d9b5bab93c6ed655363ac42e26b9
Stephen Gutekanst <stephen@hexops.com>
2022-07-24 14:58:17 -0700

    gpu: convert ShaderModule from enum(usize) to *opaque
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/shader_module.zig
+++ gpu/src/shader_module.zig
@@ -1,24 +1,19 @@
 const ChainedStruct = @import("types.zig").ChainedStruct;
 
-pub const ShaderModule = enum(usize) {
-    _,
+pub const ShaderModule = *opaque {};
 
-    // TODO: verify there is a use case for nullable value of this type.
-    pub const none: ShaderModule = @intToEnum(ShaderModule, 0);
+pub const ShaderModuleDescriptor = extern struct {
+    next_in_chain: *const ChainedStruct,
+    label: ?[*:0]const u8 = null,
+};
 
-    pub const Descriptor = extern struct {
-        next_in_chain: *const ChainedStruct,
-        label: ?[*:0]const u8 = null,
-    };
+pub const ShaderModuleSPIRVDescriptor = extern struct {
+    chain: ChainedStruct,
+    code_size: u32,
+    code: [*]const u32,
+};
 
-    pub const SPIRVDescriptor = extern struct {
-        chain: ChainedStruct,
-        code_size: u32,
-        code: [*]const u32,
-    };
-
-    pub const WGSLDescriptor = extern struct {
-        chain: ChainedStruct,
-        source: [*:0]const u8,
-    };
+pub const ShaderModuleWGSLDescriptor = extern struct {
+    chain: ChainedStruct,
+    source: [*:0]const u8,
 };
