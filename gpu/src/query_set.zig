3d0051e7202375ca2c0d36c2d1ad6d9bc033e540
Stephen Gutekanst <stephen@hexops.com>
2022-07-24 14:53:29 -0700

    gpu: convert QuerySet from enum(usize) to *opaque
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/query_set.zig
+++ gpu/src/query_set.zig
@@ -2,18 +2,13 @@ const ChainedStruct = @import("types.zig").ChainedStruct;
 const PipelineStatisticName = @import("types.zig").PipelineStatisticName;
 const QueryType = @import("types.zig").QueryType;
 
-pub const QuerySet = enum(usize) {
-    _,
+pub const QuerySet = *opaque {};
 
-    // TODO: verify there is a use case for nullable value of this type.
-    pub const none: QuerySet = @intToEnum(QuerySet, 0);
-
-    pub const Descriptor = extern struct {
-        next_in_chain: *const ChainedStruct,
-        label: ?[*:0]const u8 = null,
-        type: QueryType,
-        count: u32,
-        pipeline_statistics: [*]const PipelineStatisticName,
-        pipeline_statistics_count: u32,
-    };
+pub const QuerySetDescriptor = extern struct {
+    next_in_chain: *const ChainedStruct,
+    label: ?[*:0]const u8 = null,
+    type: QueryType,
+    count: u32,
+    pipeline_statistics: [*]const PipelineStatisticName,
+    pipeline_statistics_count: u32,
 };
