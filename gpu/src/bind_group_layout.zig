4aebe7e3ea5a683ef1da22c74415bea2a89d166f
Stephen Gutekanst <stephen@hexops.com>
2022-07-24 14:46:04 -0700

    gpu: convert BindGroupLayout from enum(usize) to *opaque
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/bind_group_layout.zig
+++ gpu/src/bind_group_layout.zig
@@ -5,26 +5,21 @@ const Sampler = @import("sampler.zig").Sampler;
 const Texture = @import("texture.zig").Texture;
 const StorageTextureBindingLayout = @import("types.zig").StorageTextureBindingLayout;
 
-pub const BindGroupLayout = enum(usize) {
-    _,
+pub const BindGroupLayout = *opaque {};
 
-    // TODO: verify there is a use case for nullable value of this type.
-    pub const none: BindGroupLayout = @intToEnum(BindGroupLayout, 0);
+pub const BindGroupLayoutEntry = extern struct {
+    next_in_chain: *const ChainedStruct,
+    binding: u32,
+    visibility: ShaderStageFlags,
+    buffer: Buffer.BindingLayout,
+    sampler: Sampler.BindingLayout,
+    texture: Texture.BindingLayout,
+    storage_texture: StorageTextureBindingLayout,
+};
 
-    pub const Entry = extern struct {
-        next_in_chain: *const ChainedStruct,
-        binding: u32,
-        visibility: ShaderStageFlags,
-        buffer: Buffer.BindingLayout,
-        sampler: Sampler.BindingLayout,
-        texture: Texture.BindingLayout,
-        storage_texture: StorageTextureBindingLayout,
-    };
-
-    pub const Descriptor = extern struct {
-        next_in_chain: *const ChainedStruct,
-        label: ?[*:0]const u8 = null,
-        entry_count: u32,
-        entries: [*]const Entry,
-    };
+pub const BindGroupLayoutDescriptor = extern struct {
+    next_in_chain: *const ChainedStruct,
+    label: ?[*:0]const u8 = null,
+    entry_count: u32,
+    entries: [*]const BindGroupLayoutEntry,
 };
