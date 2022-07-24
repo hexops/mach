601204d8bb86d0ea8be3c65e57ec6a3d98f70867
Stephen Gutekanst <stephen@hexops.com>
2022-07-24 14:50:54 -0700

    gpu: convert Device from enum(usize) to *opaque
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/device.zig
+++ gpu/src/device.zig
@@ -3,23 +3,18 @@ const FeatureName = @import("types.zig").FeatureName;
 const RequiredLimits = @import("types.zig").RequiredLimits;
 const Queue = @import("queue.zig").Queue;
 
-pub const Device = enum(usize) {
-    _,
+pub const Device = *opaque {};
 
-    // TODO: verify there is a use case for nullable value of this type.
-    pub const none: Device = @intToEnum(Device, 0);
+pub const DeviceLostReason = enum(u32) {
+    undef = 0x00000000,
+    destroyed = 0x00000001,
+};
 
-    pub const LostReason = enum(u32) {
-        undef = 0x00000000,
-        destroyed = 0x00000001,
-    };
-
-    pub const Descriptor = extern struct {
-        next_in_chain: *const ChainedStruct,
-        label: ?[*:0]const u8 = null,
-        required_features_count: u32,
-        required_features: [*]const FeatureName,
-        required_limits: ?*const RequiredLimits = null, // nullable
-        default_queue: Queue.Descriptor,
-    };
+pub const DeviceDescriptor = extern struct {
+    next_in_chain: *const ChainedStruct,
+    label: ?[*:0]const u8 = null,
+    required_features_count: u32,
+    required_features: [*]const FeatureName,
+    required_limits: ?*const RequiredLimits = null, // nullable
+    default_queue: Queue.Descriptor,
 };
