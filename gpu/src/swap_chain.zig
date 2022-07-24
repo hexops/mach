7cbac84107560000e50dc3fdaa37962acbba6e2b
Stephen Gutekanst <stephen@hexops.com>
2022-07-24 14:59:41 -0700

    gpu: convert SwapChain from enum(usize) to *opaque
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/swap_chain.zig
+++ gpu/src/swap_chain.zig
@@ -2,20 +2,15 @@ const ChainedStruct = @import("types.zig").ChainedStruct;
 const PresentMode = @import("types.zig").PresentMode;
 const Texture = @import("texture.zig").Texture;
 
-pub const SwapChain = enum(usize) {
-    _,
+pub const SwapChain = *opaque {};
 
-    // TODO: verify there is a use case for nullable value of this type.
-    pub const none: SwapChain = @intToEnum(SwapChain, 0);
-
-    pub const Descriptor = extern struct {
-        next_in_chain: *const ChainedStruct,
-        label: ?[*:0]const u8 = null,
-        usage: Texture.UsageFlags,
-        format: Texture.Format,
-        width: u32,
-        height: u32,
-        present_mode: PresentMode,
-        implementation: u64,
-    };
+pub const SwapChainDescriptor = extern struct {
+    next_in_chain: *const ChainedStruct,
+    label: ?[*:0]const u8 = null,
+    usage: Texture.UsageFlags,
+    format: Texture.Format,
+    width: u32,
+    height: u32,
+    present_mode: PresentMode,
+    implementation: u64,
 };
