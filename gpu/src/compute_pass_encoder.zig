ad1122882af8fbb6b5ed34462c7cfe059ee458d1
Stephen Gutekanst <stephen@hexops.com>
2022-07-24 14:49:50 -0700

    gpu: convert ComputePassEncoder from enum(usize) to *opaque
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/compute_pass_encoder.zig
+++ gpu/src/compute_pass_encoder.zig
@@ -1,6 +1,1 @@
-pub const ComputePassEncoder = enum(usize) {
-    _,
-
-    // TODO: verify there is a use case for nullable value of this type.
-    pub const none: ComputePassEncoder = @intToEnum(ComputePassEncoder, 0);
-};
+pub const ComputePassEncoder = *opaque {};
