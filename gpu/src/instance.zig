ec6a95e85e55f28225f573a6535492ea6f5394b4
Stephen Gutekanst <stephen@hexops.com>
2022-07-24 23:32:56 -0700

    gpu: add RequestAdapterCallback
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/instance.zig
+++ gpu/src/instance.zig
@@ -1,6 +1,15 @@
 const ChainedStruct = @import("types.zig").ChainedStruct;
+const RequestAdapterStatus = @import("types.zig").RequestAdapterStatus;
+const Adapter = @import("adapter.zig").Adapter;
 
 pub const Instance = *opaque {};
+
+pub const RequestAdapterCallback = fn (
+    status: RequestAdapterStatus,
+    adapter: Adapter,
+    message: [*:0]const u8,
+    userdata: *anyopaque,
+) callconv(.C) void;
 
 pub const InstanceDescriptor = extern struct {
     next_in_chain: *const ChainedStruct,
