d0474f39ad2c0c3e9337d5959e76389ae8292318
Stephen Gutekanst <stephen@hexops.com>
2022-07-27 10:21:10 -0700

    gpu: remove deprecated stride_undefined constant
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/main.zig
+++ gpu/src/main.zig
@@ -5,7 +5,6 @@ pub const copy_stride_undefined = 0xffffffff;
 pub const limit_u32_undefined = 0xffffffff;
 pub const limit_u64_undefined = 0xffffffffffffffff;
 pub const mip_level_count_undefined = 0xffffffff;
-pub const stride_undefined = 0xffffffff;
 pub const whole_map_size = std.math.maxInt(usize);
 pub const whole_size = 0xffffffffffffffff;
 
