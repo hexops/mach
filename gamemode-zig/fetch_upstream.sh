a89b9b1544268b9ff7b54d98c9900a7116de9eb0
PiergiorgioZagaria <pgiorgio.zagaria@gmail.com>
2022-07-28 10:38:17 +0200

    gamemode: Fix overcomplications and @import with symlinks

--- gamemode-zig/fetch_upstream.sh
+++ gamemode-zig/fetch_upstream.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
-
-rm ./c/*
-cd ./c
+rm ./gamemode_client.h
 wget "https://github.com/FeralInteractive/gamemode/raw/master/lib/gamemode_client.h"
-wget "https://github.com/FeralInteractive/gamemode/raw/master/lib/client_impl.c"
-wget "https://github.com/FeralInteractive/gamemode/raw/master/lib/client_loader.c"
-
-cd ./common
-wget "https://github.com/FeralInteractive/gamemode/raw/b11d2912e280acb87d9ad114d6c7cd8846c4ef02/common/common-helpers.c"
-wget "https://github.com/FeralInteractive/gamemode/raw/b11d2912e280acb87d9ad114d6c7cd8846c4ef02/common/common-pidfds.c"
-wget "https://github.com/FeralInteractive/gamemode/raw/b11d2912e280acb87d9ad114d6c7cd8846c4ef02/common/common-helpers.h"
-wget "https://github.com/FeralInteractive/gamemode/raw/b11d2912e280acb87d9ad114d6c7cd8846c4ef02/common/common-pidfds.h"
-
-cd ../..
 
 # The output from translate-c isn't perfect, so we need this fix
-zig translate-c ./c/gamemode_client.h -lc | sed "s#functor\(.*\)@alignCast\(.*\),\(.*\))#functor\1\3#" > ./src/gamemode_client.zig
-
-rm -r ./zig-cache
+zig translate-c ./gamemode_client.h -lc | sed "s#functor\(.*\)@alignCast\(.*\),\(.*\))#functor\1\3#" > ./gamemode_client.zig
