9d367c39576d7473b1e54913740fd6af675c64b4
Stephen Gutekanst <stephen@hexops.com>
2022-07-30 00:00:05 -0700

    gpu: internalize Surface types
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/surface.zig
+++ gpu/src/surface.zig
@@ -2,6 +2,54 @@ const ChainedStruct = @import("types.zig").ChainedStruct;
 const Impl = @import("interface.zig").Impl;
 
 pub const Surface = opaque {
+    pub const Descriptor = extern struct {
+        next_in_chain: ?*const ChainedStruct = null,
+        label: ?[*:0]const u8 = null,
+    };
+
+    pub const DescriptorFromAndroidNativeWindow = extern struct {
+        chain: ChainedStruct,
+        window: *anyopaque,
+    };
+
+    pub const DescriptorFromCanvasHTMLSelector = extern struct {
+        chain: ChainedStruct,
+        selector: [*:0]const u8,
+    };
+
+    pub const DescriptorFromMetalLayer = extern struct {
+        chain: ChainedStruct,
+        layer: *anyopaque,
+    };
+
+    pub const DescriptorFromWaylandSurface = extern struct {
+        chain: ChainedStruct,
+        display: *anyopaque,
+        surface: *anyopaque,
+    };
+
+    pub const DescriptorFromWindowsCoreWindow = extern struct {
+        chain: ChainedStruct,
+        core_window: *anyopaque,
+    };
+
+    pub const DescriptorFromWindowsHWND = extern struct {
+        chain: ChainedStruct,
+        hinstance: *anyopaque,
+        hwnd: *anyopaque,
+    };
+
+    pub const DescriptorFromWindowsSwapChainPanel = extern struct {
+        chain: ChainedStruct,
+        swap_chain_panel: *anyopaque,
+    };
+
+    pub const DescriptorFromXlibWindow = extern struct {
+        chain: ChainedStruct,
+        display: *anyopaque,
+        window: u32,
+    };
+
     pub inline fn reference(surface: *Surface) void {
         Impl.surfaceReference(surface);
     }
@@ -9,52 +57,4 @@ pub const Surface = opaque {
     pub inline fn release(surface: *Surface) void {
         Impl.surfaceRelease(surface);
     }
-};
-
-pub const SurfaceDescriptor = extern struct {
-    next_in_chain: ?*const ChainedStruct = null,
-    label: ?[*:0]const u8 = null,
-};
-
-pub const SurfaceDescriptorFromAndroidNativeWindow = extern struct {
-    chain: ChainedStruct,
-    window: *anyopaque,
-};
-
-pub const SurfaceDescriptorFromCanvasHTMLSelector = extern struct {
-    chain: ChainedStruct,
-    selector: [*:0]const u8,
-};
-
-pub const SurfaceDescriptorFromMetalLayer = extern struct {
-    chain: ChainedStruct,
-    layer: *anyopaque,
-};
-
-pub const SurfaceDescriptorFromWaylandSurface = extern struct {
-    chain: ChainedStruct,
-    display: *anyopaque,
-    surface: *anyopaque,
-};
-
-pub const SurfaceDescriptorFromWindowsCoreWindow = extern struct {
-    chain: ChainedStruct,
-    core_window: *anyopaque,
-};
-
-pub const SurfaceDescriptorFromWindowsHWND = extern struct {
-    chain: ChainedStruct,
-    hinstance: *anyopaque,
-    hwnd: *anyopaque,
-};
-
-pub const SurfaceDescriptorFromWindowsSwapChainPanel = extern struct {
-    chain: ChainedStruct,
-    swap_chain_panel: *anyopaque,
-};
-
-pub const SurfaceDescriptorFromXlibWindow = extern struct {
-    chain: ChainedStruct,
-    display: *anyopaque,
-    window: u32,
 };
