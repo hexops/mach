223f10446afde72d39d5b365c6ac13ed3376732b
Stephen Gutekanst <stephen@hexops.com>
2022-07-29 22:11:13 -0700

    gpu: convert *opaque -> opaque for Instance
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/instance.zig
+++ gpu/src/instance.zig
@@ -6,20 +6,20 @@ const Adapter = @import("adapter.zig").Adapter;
 const RequestAdapterOptions = @import("main.zig").RequestAdapterOptions;
 const Impl = @import("interface.zig").Impl;
 
-pub const Instance = *opaque {
-    pub inline fn createSurface(instance: Instance, descriptor: *const SurfaceDescriptor) Surface {
+pub const Instance = opaque {
+    pub inline fn createSurface(instance: *Instance, descriptor: *const SurfaceDescriptor) Surface {
         return Impl.instanceCreateSurface(instance, descriptor);
     }
 
-    pub inline fn requestAdapter(instance: Instance, options: *const RequestAdapterOptions, callback: RequestAdapterCallback, userdata: *anyopaque) void {
+    pub inline fn requestAdapter(instance: *Instance, options: *const RequestAdapterOptions, callback: RequestAdapterCallback, userdata: *anyopaque) void {
         Impl.instanceRequestAdapter(instance, options, callback, userdata);
     }
 
-    pub inline fn reference(instance: Instance) void {
+    pub inline fn reference(instance: *Instance) void {
         Impl.instanceReference(instance);
     }
 
-    pub inline fn release(instance: Instance) void {
+    pub inline fn release(instance: *Instance) void {
         Impl.instanceRelease(instance);
     }
 };
