6692be5110777b3fa8d104971b324d7a1dad4ec6
Stephen Gutekanst <stephen@hexops.com>
2022-07-29 22:57:21 -0700

    gpu: convert *opaque -> opaque for RenderBundle
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/render_bundle.zig
+++ gpu/src/render_bundle.zig
@@ -2,11 +2,11 @@ const ChainedStruct = @import("types.zig").ChainedStruct;
 const Impl = @import("interface.zig").Impl;
 
 pub const RenderBundle = *opaque {
-    pub inline fn reference(render_bundle: RenderBundle) void {
+    pub inline fn reference(render_bundle: *RenderBundle) void {
         Impl.renderBundleReference(render_bundle);
     }
 
-    pub inline fn release(render_bundle: RenderBundle) void {
+    pub inline fn release(render_bundle: *RenderBundle) void {
         Impl.renderBundleRelease(render_bundle);
     }
 };
