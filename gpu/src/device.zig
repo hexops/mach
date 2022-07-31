ab337066a0f768dce66b02af923cfdde4f3fddcd
Stephen Gutekanst <stephen@hexops.com>
2022-07-31 11:03:48 -0700

    gpu: make Device.setUncapturedErrorCallback friendlier
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/src/device.zig
+++ gpu/src/device.zig
@@ -194,10 +194,16 @@ pub const Device = opaque {
     // TODO: presumably callback should be nullable for unsetting
     pub inline fn setUncapturedErrorCallback(
         device: *Device,
-        callback: ErrorCallback,
-        userdata: ?*anyopaque,
+        comptime Context: type,
+        comptime callback: fn (typ: ErrorType, message: [*:0]const u8, ctx: Context) callconv(.Inline) void,
+        context: Context,
     ) void {
-        Impl.deviceSetUncapturedErrorCallback(device, callback, userdata);
+        const Helper = struct {
+            pub fn callback(typ: ErrorType, message: [*:0]const u8, userdata: ?*anyopaque) callconv(.C) void {
+                callback(typ, message, if (Context == void) {} orelse @ptrCast(Context, userdata));
+            }
+        };
+        Impl.deviceSetUncapturedErrorCallback(device, Helper.callback, if (Context == void) null orelse context);
     }
 
     pub inline fn tick(device: *Device) void {
