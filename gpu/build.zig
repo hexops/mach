6c401b6067ea30e2f030d9fe56ce9f30cac29358
Stephen Gutekanst <stephen@hexops.com>
2022-08-19 12:04:33 -0700

    gpu: decouple SDK from dependencies
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- gpu/build.zig
+++ gpu/build.zig
@@ -1,61 +1,39 @@
 const std = @import("std");
-const gpu_dawn = @import("libs/mach-gpu-dawn/build.zig");
 const glfw = @import("libs/mach-glfw/build.zig");
+const gpu_dawn_sdk = @import("libs/mach-gpu-dawn/sdk.zig");
+const gpu_sdk = @import("sdk.zig");
+const system_sdk = @import("libs/mach-glfw/system_sdk.zig");
 
 pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
     const target = b.standardTargetOptions(.{});
+    const gpu_dawn = gpu_dawn_sdk.Sdk(.{
+        .glfw = glfw,
+        .glfw_include_dir = "libs/mach-glfw/upstream/glfw/include",
+        .system_sdk = system_sdk,
+    });
+    const gpu = gpu_sdk.Sdk(.{
+        .glfw = glfw,
+        .gpu_dawn = gpu_dawn,
+    });
 
     const gpu_dawn_options = gpu_dawn.Options{
         .from_source = b.option(bool, "dawn-from-source", "Build Dawn from source") orelse false,
     };
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&testStep(b, mode, target, .{ .gpu_dawn_options = gpu_dawn_options }).step);
+    test_step.dependOn(&gpu.testStep(b, mode, target, .{ .gpu_dawn_options = gpu_dawn_options }).step);
 
     const example = b.addExecutable("gpu-hello-triangle", "examples/main.zig");
     example.setBuildMode(mode);
     example.setTarget(target);
-    example.addPackage(pkg);
+    example.addPackage(gpu.pkg);
     example.addPackage(glfw.pkg);
-    link(b, example, .{ .gpu_dawn_options = gpu_dawn_options });
+    gpu.link(b, example, .{ .gpu_dawn_options = gpu_dawn_options });
     example.install();
 
     const example_run_cmd = example.run();
     example_run_cmd.step.dependOn(b.getInstallStep());
     const example_run_step = b.step("run-example", "Run the example");
     example_run_step.dependOn(&example_run_cmd.step);
-}
-
-pub fn testStep(b: *std.build.Builder, mode: std.builtin.Mode, target: std.zig.CrossTarget, options: Options) *std.build.RunStep {
-    const main_tests = b.addTestExe("gpu-tests", (comptime thisDir()) ++ "/src/main.zig");
-    main_tests.setBuildMode(mode);
-    main_tests.setTarget(target);
-    link(b, main_tests, options);
-    main_tests.install();
-    return main_tests.run();
-}
-
-pub const Options = struct {
-    glfw_options: glfw.Options = .{},
-    gpu_dawn_options: gpu_dawn.Options = .{},
-};
-
-pub const pkg = std.build.Pkg{
-    .name = "gpu",
-    .source = .{ .path = thisDir() ++ "/src/main.zig" },
-    .dependencies = &.{glfw.pkg},
-};
-
-pub fn link(b: *std.build.Builder, step: *std.build.LibExeObjStep, options: Options) void {
-    if (step.target.toTarget().cpu.arch != .wasm32) {
-        glfw.link(b, step, options.glfw_options);
-        gpu_dawn.link(b, step, options.gpu_dawn_options);
-        step.addCSourceFile((comptime thisDir()) ++ "/src/mach_dawn.cpp", &.{"-std=c++17"});
-        step.addIncludeDir((comptime thisDir()) ++ "/src");
-    }
-}
-
-fn thisDir() []const u8 {
-    return std.fs.path.dirname(@src().file) orelse ".";
 }
---
+++ gpu/sdk.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+// const gpu_dawn = @import("libs/mach-gpu-dawn/build.zig");
+// const glfw = @import("libs/mach-glfw/build.zig");
+
+pub fn Sdk(deps: anytype) type {
+    return struct {
+        pub fn testStep(b: *std.build.Builder, mode: std.builtin.Mode, target: std.zig.CrossTarget, options: Options) *std.build.RunStep {
+            const main_tests = b.addTestExe("gpu-tests", (comptime thisDir()) ++ "/src/main.zig");
+            main_tests.setBuildMode(mode);
+            main_tests.setTarget(target);
+            link(b, main_tests, options);
+            main_tests.install();
+            return main_tests.run();
+        }
+
+        pub const Options = struct {
+            glfw_options: deps.glfw.Options = .{},
+            gpu_dawn_options: deps.gpu_dawn.Options = .{},
+        };
+
+        pub const pkg = std.build.Pkg{
+            .name = "gpu",
+            .source = .{ .path = thisDir() ++ "/src/main.zig" },
+            .dependencies = &.{deps.glfw.pkg},
+        };
+
+        pub fn link(b: *std.build.Builder, step: *std.build.LibExeObjStep, options: Options) void {
+            if (step.target.toTarget().cpu.arch != .wasm32) {
+                deps.glfw.link(b, step, options.glfw_options);
+                deps.gpu_dawn.link(b, step, options.gpu_dawn_options);
+                step.addCSourceFile((comptime thisDir()) ++ "/src/mach_dawn.cpp", &.{"-std=c++17"});
+                step.addIncludeDir((comptime thisDir()) ++ "/src");
+            }
+        }
+
+        fn thisDir() []const u8 {
+            return std.fs.path.dirname(@src().file) orelse ".";
+        }
+    };
+}
