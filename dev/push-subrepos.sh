b6a512ebaccc3fa7b67fdf24c6a65ff114120767
Stephen Gutekanst <stephen@hexops.com>
2022-08-23 10:35:28 -0700

    dev: update subproject paths to have libs/ prefix
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- dev/push-subrepos.sh
+++ dev/push-subrepos.sh
@@ -3,6 +3,6 @@ set -exuo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
 ./dev/update-subrepos.sh
-git subtree push --prefix glfw mach-glfw main
-git subtree push --prefix gpu-dawn mach-gpu-dawn main
-git subtree push --prefix freetype mach-freetype main
+git subtree push --prefix libs/glfw mach-glfw main
+git subtree push --prefix libs/gpu-dawn mach-gpu-dawn main
+git subtree push --prefix libs/freetype mach-freetype main
