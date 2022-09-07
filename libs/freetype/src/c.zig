ad981944d40391c9951af4a2e8b6612ac3f284fe
Ali Chraghi <alichraghi@pm.me>
2022-09-07 23:48:43 +0430

    all: revert cimport workaround

--- libs/freetype/src/c.zig
+++ libs/freetype/src/c.zig
@@ -1,17 +1,10 @@
-pub usingnamespace if (@import("builtin").zig_backend == .stage1 or !@import("builtin").target.isDarwin())
-    @cImport({
-        @cInclude("freetype/ftadvanc.h");
-        @cInclude("freetype/ftbbox.h");
-        @cInclude("freetype/ftbitmap.h");
-        @cInclude("freetype/ftcolor.h");
-        @cInclude("freetype/ftlcdfil.h");
-        @cInclude("freetype/ftsizes.h");
-        @cInclude("freetype/ftstroke.h");
-        @cInclude("freetype/fttrigon.h");
-    })
-else
-    // TODO(self-hosted): HACK: workaround https://github.com/ziglang/zig/issues/12483
-    //
-    // Extracted from a build using stage1 from zig-cache/ (`cimport.zig`)
-    // Then find+replace `= ?fn` -> `= ?*const fn`
-    @import("cimport1.zig");
+pub usingnamespace @cImport({
+    @cInclude("freetype/ftadvanc.h");
+    @cInclude("freetype/ftbbox.h");
+    @cInclude("freetype/ftbitmap.h");
+    @cInclude("freetype/ftcolor.h");
+    @cInclude("freetype/ftlcdfil.h");
+    @cInclude("freetype/ftsizes.h");
+    @cInclude("freetype/ftstroke.h");
+    @cInclude("freetype/fttrigon.h");
+});
--- libs/freetype/src/harfbuzz/c.zig
+++ libs/freetype/src/harfbuzz/c.zig
@@ -1,8 +1,1 @@
-pub usingnamespace if (@import("builtin").zig_backend == .stage1 or !@import("builtin").target.isDarwin())
-    @cImport(@cInclude("hb-ft.h"))
-else
-    // TODO(self-hosted): HACK: workaround https://github.com/ziglang/zig/issues/12483
-    //
-    // Extracted from a build using stage1 from zig-cache/ (`cimport.zig`)
-    // Then find+replace `= ?fn` -> `= ?*const fn`
-    @import("cimport1.zig");
+pub usingnamespace @cImport(@cInclude("hb-ft.h"));
