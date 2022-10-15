373c55894f6d092dbc5bbd2c09d7accebd863532
Stephen Gutekanst <stephen@hexops.com>
2022-10-15 04:04:57 -0700

    {dev,gpu-dawn}: add yamlfmt to ci-lint.sh
    Fixes hexops/mach#430
    
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- dev/ci-lint.sh
+++ dev/ci-lint.sh
@@ -3,4 +3,7 @@ set -exuo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
 # actionlint: https://github.com/rhysd/actionlint
-./actionlint $(find . | grep -v '\.git/' | grep '\.github/workflows/' | grep '.yml')
+actionlint $(find . | grep -v '\.git/' | grep '\.github/workflows/' | grep '.yml')
+
+# yamlfmt: https://github.com/google/yamlfmt
+yamlfmt '**/.github/**/*.yml' '**/.github/**/*.yaml'
