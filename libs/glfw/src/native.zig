d7fa9ed987d4d108465bd24d4e2aeb140756e27c
dweiller <4678790+dweiller@users.noreplay.github.com>
2022-10-16 12:27:02 +1100

    glfw: fix native import

--- libs/glfw/src/native.zig
+++ libs/glfw/src/native.zig
@@ -40,7 +40,7 @@ pub const BackendOptions = struct {
 /// The chosen backends must match those the library was compiled for. Failure to do so
 /// will cause a link-time error.
 pub fn Native(comptime options: BackendOptions) type {
-    const native = @import("c_native.zig").import(options);
+    const native = @import("cimport/c_normal_native.zig").import(options);
 
     return struct {
         /// Returns the adapter device name of the specified monitor.
