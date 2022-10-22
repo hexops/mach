db4743b2ed137ed5329e3cbf21869b1b600a1d1c
Stephen Gutekanst <stephen@hexops.com>
2022-10-22 09:10:41 -0700

    dev: also update mach-examples zig version
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- dev/update-zig.sh
+++ dev/update-zig.sh
@@ -24,6 +24,10 @@ if [ -n "${ZIG_VERSION:-}" ]; then
     
     sources=$(find . | grep './.github/workflows' | grep -v 'third_party/' | grep -v 'DirectXShaderCompiler' | grep '\.yml')
     echo "$sources" | while read line ; do update_zig "$version" "$line" ; done
+
+    # If running inside mach-examples/libs/mach, also update the mach-examples repo.
+    sources=$(find ../../.github/ | grep '/workflows/' | grep '\.yml')
+    echo "$sources" | while read line ; do update_zig "$version" "$line" ; done
 else
     echo "must specify e.g. ZIG_VERSION=0.10.0-dev.2017+a0a2ce92c"
     exit 0
