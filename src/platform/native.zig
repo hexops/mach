91a53807ab72a740abb0c500927cdcb390890682
Lue <lue@lued.me>
2023-01-11 00:26:21 +0000

    mach: fix outdated reference to `glfw.Error`

--- src/platform/native.zig
+++ src/platform/native.zig
@@ -575,7 +575,7 @@ pub const Platform = struct {
     }
 
     /// Default GLFW error handling callback
-    fn errorCallback(error_code: glfw.Error, description: [:0]const u8) void {
+    fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
         std.log.err("glfw: {}: {s}\n", .{ error_code, description });
     }
 };
