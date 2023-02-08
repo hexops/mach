d348875dac7a2a734fe3e41f4899a3e97655336f
Stephen Gutekanst <stephen@hexops.com>
2023-02-08 01:38:55 -0700

    gpu: update to latest Zig build API
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- libs/gpu/sdk.zig
+++ libs/gpu/sdk.zig
@@ -2,10 +2,14 @@ const std = @import("std");
 
 pub fn Sdk(comptime deps: anytype) type {
     return struct {
-        pub fn testStep(b: *std.build.Builder, mode: std.builtin.Mode, target: std.zig.CrossTarget, options: Options) !*std.build.RunStep {
-            const main_tests = b.addTestExe("gpu-tests", sdkPath("/src/main.zig"));
-            main_tests.setBuildMode(mode);
-            main_tests.setTarget(target);
+        pub fn testStep(b: *std.Build, optimize: std.builtin.OptimizeMode, target: std.zig.CrossTarget, options: Options) !*std.build.RunStep {
+            const main_tests = b.addTest(.{
+                .name = "gpu-tests",
+                .kind = .test_exe,
+                .root_source_file = .{ .path = sdkPath("/src/main.zig") },
+                .target = target,
+                .optimize = optimize,
+            });
             try link(b, main_tests, options);
             main_tests.install();
             return main_tests.run();
@@ -15,12 +19,13 @@ pub fn Sdk(comptime deps: anytype) type {
             gpu_dawn_options: deps.gpu_dawn.Options = .{},
         };
 
-        pub const pkg = std.build.Pkg{
-            .name = "gpu",
-            .source = .{ .path = sdkPath("/src/main.zig") },
-        };
+        pub fn module(b: *std.Build) *std.build.Module {
+            return b.createModule(.{
+                .source_file = .{ .path = sdkPath("/src/main.zig") },
+            });
+        }
 
-        pub fn link(b: *std.build.Builder, step: *std.build.LibExeObjStep, options: Options) !void {
+        pub fn link(b: *std.Build, step: *std.build.CompileStep, options: Options) !void {
             if (step.target.toTarget().cpu.arch != .wasm32) {
                 try deps.gpu_dawn.link(b, step, options.gpu_dawn_options);
                 step.addCSourceFile(sdkPath("/src/mach_dawn.cpp"), &.{"-std=c++17"});
