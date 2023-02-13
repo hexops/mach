5ae82c42d41fe10fc9fe5fa1c84454e1f85c0a61
Stephen Gutekanst <stephen@hexops.com>
2023-02-12 17:30:23 -0700

    core: fix linux build (gamemode import not available during testing currently)
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- libs/core/src/main.zig
+++ libs/core/src/main.zig
@@ -3,7 +3,3 @@ pub const Core = @import("Core.zig");
 pub const Timer = @import("Timer.zig");
 pub const gpu = @import("gpu");
 pub const sysjs = @import("sysjs");
-
-test {
-    _ = @import("platform/libmachcore.zig");
-}
