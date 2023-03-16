1e3c0667baf722bd6540984fb3c1c5a3bb0fd8a2
Release automation <stephen@hexops.com>
2023-03-16 17:53:05 +0000

    gpu-dawn: update to latest binary release

--- libs/gpu-dawn/sdk.zig
+++ libs/gpu-dawn/sdk.zig
@@ -37,7 +37,7 @@ pub fn Sdk(comptime deps: anytype) type {
             install_libs: bool = false,
 
             /// The binary release version to use from https://github.com/hexops/mach-gpu-dawn/releases
-            binary_version: []const u8 = "release-4d69008",
+            binary_version: []const u8 = "release-baabbf7",
 
             /// Detects the default options to use for the given target.
             pub fn detectDefaults(self: Options, target: std.Target) Options {
