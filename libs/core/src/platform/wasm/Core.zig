67464413550c70a52844b4338154797c278d6e4f
Ali Chraghi <alichraghi@pm.me>
2023-03-28 15:41:11 +0330

    core: fix event iterator

--- libs/core/src/platform/wasm/Core.zig
+++ libs/core/src/platform/wasm/Core.zig
@@ -22,114 +22,118 @@ pub const Core = @This();
 allocator: std.mem.Allocator,
 id: js.CanvasId,
 
-last_cursor_position: Position,
-last_key_mods: KeyMods,
-
 pub const EventIterator = struct {
-    core: *Core,
+    key_mods: KeyMods,
+    last_cursor_position: Position,
 
     pub inline fn next(self: *EventIterator) ?Event {
-        const event_int = js.machEventShift();
-        if (event_int == -1) return null;
+        while (true) {
+            const event_int = js.machEventShift();
+            if (event_int == -1) return null;
 
-        const event_type = @intToEnum(std.meta.Tag(Event), event_int);
-        return switch (event_type) {
-            .key_press, .key_repeat => blk: {
-                const key = @intToEnum(Key, js.machEventShift());
-                switch (key) {
-                    .left_shift, .right_shift => self.last_key_mods.shift = true,
-                    .left_control, .right_control => self.last_key_mods.control = true,
-                    .left_alt, .right_alt => self.last_key_mods.alt = true,
-                    .left_super, .right_super => self.last_key_mods.super = true,
-                    .caps_lock => self.last_key_mods.caps_lock = true,
-                    .num_lock => self.last_key_mods.num_lock = true,
-                    else => {},
-                }
-                break :blk switch (event_type) {
-                    .key_press => Event{
-                        .key_press = .{
-                            .key = key,
-                            .mods = self.last_key_mods,
+            const event_type = @intToEnum(std.meta.Tag(Event), event_int);
+            return switch (event_type) {
+                .key_press, .key_repeat => blk: {
+                    const key = @intToEnum(Key, js.machEventShift());
+                    switch (key) {
+                        .left_shift, .right_shift => self.key_mods.shift = true,
+                        .left_control, .right_control => self.key_mods.control = true,
+                        .left_alt, .right_alt => self.key_mods.alt = true,
+                        .left_super, .right_super => self.key_mods.super = true,
+                        .caps_lock => self.key_mods.caps_lock = true,
+                        .num_lock => self.key_mods.num_lock = true,
+                        else => {
+                            break :blk switch (event_type) {
+                                .key_press => Event{
+                                    .key_press = .{
+                                        .key = key,
+                                        .mods = self.key_mods,
+                                    },
+                                },
+                                .key_repeat => Event{
+                                    .key_repeat = .{
+                                        .key = key,
+                                        .mods = self.key_mods,
+                                    },
+                                },
+                                else => unreachable,
+                            };
                         },
-                    },
-                    .key_repeat => Event{
-                        .key_repeat = .{
-                            .key = key,
-                            .mods = self.last_key_mods,
+                    }
+                    continue;
+                },
+                .key_release => blk: {
+                    const key = @intToEnum(Key, js.machEventShift());
+                    switch (key) {
+                        .left_shift, .right_shift => self.key_mods.shift = false,
+                        .left_control, .right_control => self.key_mods.control = false,
+                        .left_alt, .right_alt => self.key_mods.alt = false,
+                        .left_super, .right_super => self.key_mods.super = false,
+                        .caps_lock => self.key_mods.caps_lock = false,
+                        .num_lock => self.key_mods.num_lock = false,
+                        else => {
+                            break :blk Event{
+                                .key_release = .{
+                                    .key = key,
+                                    .mods = self.key_mods,
+                                },
+                            };
                         },
-                    },
-                    else => unreachable,
-                };
-            },
-            .key_release => blk: {
-                const key = @intToEnum(Key, js.machEventShift());
-                switch (key) {
-                    .left_shift, .right_shift => self.last_key_mods.shift = false,
-                    .left_control, .right_control => self.last_key_mods.control = false,
-                    .left_alt, .right_alt => self.last_key_mods.alt = false,
-                    .left_super, .right_super => self.last_key_mods.super = false,
-                    .caps_lock => self.last_key_mods.caps_lock = false,
-                    .num_lock => self.last_key_mods.num_lock = false,
-                    else => {},
-                }
-                break :blk Event{
-                    .key_release = .{
-                        .key = key,
-                        .mods = self.last_key_mods,
-                    },
-                };
-            },
-            .mouse_motion => blk: {
-                const x = @intToFloat(f64, js.machEventShift());
-                const y = @intToFloat(f64, js.machEventShift());
-                self.last_cursor_position = .{
-                    .x = x,
-                    .y = y,
-                };
-                break :blk Event{
-                    .mouse_motion = .{
-                        .pos = .{
-                            .x = x,
-                            .y = y,
+                    }
+                    continue;
+                },
+                .mouse_motion => blk: {
+                    const x = @intToFloat(f64, js.machEventShift());
+                    const y = @intToFloat(f64, js.machEventShift());
+                    self.last_cursor_position = .{
+                        .x = x,
+                        .y = y,
+                    };
+                    break :blk Event{
+                        .mouse_motion = .{
+                            .pos = .{
+                                .x = x,
+                                .y = y,
+                            },
                         },
+                    };
+                },
+                .mouse_press => Event{
+                    .mouse_press = .{
+                        .button = toMachButton(js.machEventShift()),
+                        .pos = self.last_cursor_position,
+                        .mods = self.key_mods,
                     },
-                };
-            },
-            .mouse_press => Event{
-                .mouse_press = .{
-                    .button = toMachButton(js.machEventShift()),
-                    .pos = self.last_cursor_position,
-                    .mods = self.last_key_mods,
                 },
-            },
-            .mouse_release => Event{
-                .mouse_release = .{
-                    .button = toMachButton(js.machEventShift()),
-                    .pos = self.last_cursor_position,
-                    .mods = self.last_key_mods,
-                },
-            },
-            .mouse_scroll => Event{
-                .mouse_scroll = .{
-                    .xoffset = @floatCast(f32, std.math.sign(js.machEventShiftFloat())),
-                    .yoffset = @floatCast(f32, std.math.sign(js.machEventShiftFloat())),
-                },
-            },
-            .framebuffer_resize => blk: {
-                const width = @intCast(u32, js.machEventShift());
-                const height = @intCast(u32, js.machEventShift());
-                const pixel_ratio = @intCast(u32, js.machEventShift());
-                break :blk Event{
-                    .framebuffer_resize = .{
-                        .width = width * pixel_ratio,
-                        .height = height * pixel_ratio,
+                .mouse_release => Event{
+                    .mouse_release = .{
+                        .button = toMachButton(js.machEventShift()),
+                        .pos = self.last_cursor_position,
+                        .mods = self.key_mods,
                     },
-                };
-            },
-            .focus_gained => Event.focus_gained,
-            .focus_lost => Event.focus_lost,
-            else => null,
-        };
+                },
+                .mouse_scroll => Event{
+                    .mouse_scroll = .{
+                        .xoffset = @floatCast(f32, std.math.sign(js.machEventShiftFloat())),
+                        .yoffset = @floatCast(f32, std.math.sign(js.machEventShiftFloat())),
+                    },
+                },
+                .framebuffer_resize => blk: {
+                    const width = @intCast(u32, js.machEventShift());
+                    const height = @intCast(u32, js.machEventShift());
+                    const pixel_ratio = @intCast(u32, js.machEventShift());
+                    break :blk Event{
+                        .framebuffer_resize = .{
+                            .width = width * pixel_ratio,
+                            .height = height * pixel_ratio,
+                        },
+                    };
+                },
+                .focus_gained => Event.focus_gained,
+                .focus_lost => Event.focus_lost,
+                else => null,
+            };
+        }
     }
 };
 
@@ -141,20 +145,6 @@ pub fn init(core: *Core, allocator: std.mem.Allocator, options: Options) !void {
     core.* = Core{
         .allocator = allocator,
         .id = id,
-
-        // TODO initialize these properly
-        .last_cursor_position = .{
-            .x = 0,
-            .y = 0,
-        },
-        .last_key_mods = .{
-            .shift = false,
-            .control = false,
-            .alt = false,
-            .super = false,
-            .caps_lock = false,
-            .num_lock = false,
-        },
     };
 }
 
@@ -163,7 +153,21 @@ pub fn deinit(self: *Core) void {
 }
 
 pub inline fn pollEvents(self: *Core) EventIterator {
-    return EventIterator{ .core = self };
+    _ = self;
+    return EventIterator{
+        .key_mods = .{
+            .shift = false,
+            .control = false,
+            .alt = false,
+            .super = false,
+            .caps_lock = false,
+            .num_lock = false,
+        },
+        .last_cursor_position = .{
+            .x = 0,
+            .y = 0,
+        },
+    };
 }
 
 pub fn framebufferSize(self: *Core) Size {
