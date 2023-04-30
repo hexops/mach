f2a904e17aa8c0a8e85cf960ae1222e90349fd7e
Stephen Gutekanst <stephen@hexops.com>
2023-04-30 16:57:19 -0700

    sysaudio: fix broken sysjs dependency include
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- libs/sysaudio/sdk.zig
+++ libs/sysaudio/sdk.zig
@@ -16,9 +16,7 @@ pub fn Sdk(comptime deps: anytype) type {
             _module = b.createModule(.{
                 .source_file = .{ .path = sdkPath("/src/main.zig") },
                 .dependencies = &.{
-                    .{ .name = "sysjs", .module = b.createModule(.{
-                        .source_file = .{ .path = "libs/mach-sysjs/src/main.zig" },
-                    }) },
+                    .{ .name = "sysjs", .module = deps.sysjs.module(b) },
                 },
             });
             return _module.?;
