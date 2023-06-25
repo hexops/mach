d1155906844813def4df19b639677b99f0c24778
Stephen Gutekanst <stephen@hexops.com>
2023-06-24 19:06:51 -0700

    dev: remove scripts that should move into wrench
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- dev/ci-lint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -exuo pipefail
-cd "$(dirname "${BASH_SOURCE[0]}")"/..
-
-# actionlint: https://github.com/rhysd/actionlint
-actionlint $(find . | grep -v '\.git/' | grep '\.github/workflows/' | grep '.yml')
-
-# yamlfmt: https://github.com/google/yamlfmt
-yamlfmt '**/.github/**/*.yml' '**/.github/**/*.yaml'
