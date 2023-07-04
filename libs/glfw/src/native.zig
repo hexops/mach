cb06d18f74a04641b8d096199d711e6010a3604b
Stephen Gutekanst <stephen@hexops.com>
2023-07-03 20:16:14 -0700

    glfw: fix glfw.Native usage on Linux
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- libs/glfw/src/native.zig
+++ libs/glfw/src/native.zig
@@ -40,6 +40,7 @@ pub const BackendOptions = struct {
 pub fn Native(comptime options: BackendOptions) type {
     const native = @cImport({
         @cDefine("GLFW_INCLUDE_VULKAN", "1");
+        @cDefine("GLFW_INCLUDE_NONE", "1");
         if (options.win32) @cDefine("GLFW_EXPOSE_NATIVE_WIN32", "1");
         if (options.wgl) @cDefine("GLFW_EXPOSE_NATIVE_WGL", "1");
         if (options.cocoa) @cDefine("GLFW_EXPOSE_NATIVE_COCOA", "1");
