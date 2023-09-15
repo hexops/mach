d1da91666a3aaef232275a6629a355026feafb58
Olle Lukowski <ollelukowski@hotmail.com>
2023-09-15 11:15:51 +0200

    fromAxisAngle

--- src/math/quat.zig
+++ src/math/quat.zig
@@ -15,8 +15,19 @@ pub fn Quat(comptime Scalar: type) type {
         /// The underlying Vec type, e.g. math.Vec4, math.Vec4h, math.Vec4d
         pub const Vec = vec.Vec(4, Scalar);
 
+        /// The Vec type used to represent axes, e.g. math.Vec3
+        pub const Axis = vec.Vec(3, Scalar);
+
         pub inline fn init(x: T, y: T, z: T, w: T) Quat(Scalar) {
             return .{ .v = math.vec4(x, y, z, w) };
+        }
+
+        /// Creates a Quaternion based on the given `axis` and `angle`, and returns it.
+        pub inline fn fromAxisAngle(axis: Axis, angle: T) Quat(Scalar) {
+            const halfAngle = angle * 0.5;
+            const s = std.math.sin(halfAngle);
+
+            return init(s * axis[0], s * axis[1], s * axis[2], std.math.cos(halfAngle));
         }
     };
 }
@@ -31,4 +42,11 @@ test "init" {
     try testing.expect(math.Quat, math.quat(1, 2, 3, 4)).eql(math.Quat{
         .v = math.vec4(1, 2, 3, 4),
     });
+}
+
+test "fromAxisAngle" {
+    const expected = math.Quat.init(0.383, 0.0, 0.0, 0.924); // Expected values for a 45-degree rotation around the x-axis
+    const actual = math.Quat.fromAxisAngle(math.Axis{ .x = 1.0 }, 0.785398); // 45 degrees in radians (π/4) around the x-axis
+
+    try testing.expect(math.Quat, actual).eql(expected);
 }
