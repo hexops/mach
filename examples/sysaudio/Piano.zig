46ba3bdb2e2dc23a8acb28b1450a4692b1d4d5ec
Ali Chraghi <alichraghi@proton.me>
2024-01-07 18:49:26 +0330

    add audio module

---
+++ examples/sysaudio/Piano.zig
@@ -0,0 +1,151 @@
+// A simple tone engine.
+//
+// `keyToFrequency` can be used to convert a keyboard key to a frequency, so that the
+// keys asdfghj on your QWERTY keyboard will map to the notes C/D/E/F/G/A/B[4], the
+// keys above qwertyu will map to C5 and the keys below zxcvbnm will map to C3.
+//
+// The duration is hard-coded to 1.5s. To prevent clicking, tones are faded in linearly over
+// the first 1/64th duration of the tone. To provide a cool sustained effect, tones are faded
+// out using 1-log10(x*10) (google it to see how it looks, it's strong for most of the duration of
+// the note then fades out slowly.)
+const std = @import("std");
+const builtin = @import("builtin");
+
+const mach = @import("mach");
+const math = mach.math;
+const sysaudio = mach.sysaudio;
+
+pub const App = @This();
+
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
+pub const name = .piano;
+pub const Mod = mach.Mod(@This());
+
+pub const global_events = .{
+    .init = .{ .handler = init },
+    .tick = .{ .handler = tick },
+};
+
+tones: std.AutoHashMapUnmanaged(mach.core.Key, mach.EntityID) = .{},
+
+fn init(audio: *mach.Audio.Mod, piano: *Mod) !void {
+    audio.init(.{ .allocator = gpa.allocator() });
+    try audio.state().init();
+
+    piano.init(.{});
+    inline for (@typeInfo(mach.core.Key).Enum.fields) |field| {
+        const key: mach.core.Key = @enumFromInt(field.value);
+        const entity = try audio.newEntity();
+        try audio.set(entity, .samples, try fillTone(audio, key));
+        try audio.set(entity, .playing, false);
+        try audio.set(entity, .index, 0);
+        try piano.state().tones.put(gpa.allocator(), key, entity);
+    }
+}
+
+pub fn tick(
+    engine: *mach.Engine.Mod,
+    audio: *mach.Audio.Mod,
+    piano: *Mod,
+) !void {
+    var iter = mach.core.pollEvents();
+    while (iter.next()) |event| {
+        switch (event) {
+            .key_press => |ev| {
+                const vol = try audio.state().player.volume();
+                switch (ev.key) {
+                    .down => try audio.state().player.setVolume(@max(0.0, vol - 0.1)),
+                    .up => try audio.state().player.setVolume(@min(1.0, vol + 0.1)),
+                    else => {
+                        const entity = piano.state().tones.get(ev.key).?;
+                        try audio.set(entity, .playing, true);
+                    },
+                }
+            },
+            .close => engine.send(.exit, .{}),
+            else => {},
+        }
+    }
+
+    audio.send(.render, .{});
+
+    const back_buffer_view = mach.core.swap_chain.getCurrentTextureView().?;
+
+    mach.core.swap_chain.present();
+    back_buffer_view.release();
+}
+
+pub fn fillTone(audio: *mach.Audio.Mod, key: mach.core.Key) ![]const f32 {
+    const frequency = keyToFrequency(key);
+    const channels = audio.state().player.channels().len;
+    const sample_rate: f32 = @floatFromInt(audio.state().player.sampleRate());
+    const duration: f32 = 1.5 * @as(f32, @floatFromInt(channels)) * sample_rate; // play the tone for 1.5s
+    const gain = 0.1;
+
+    const samples = try gpa.allocator().alloc(f32, @intFromFloat(duration));
+
+    var i: usize = 0;
+    while (i < samples.len) : (i += channels) {
+        const sample_index: f32 = @floatFromInt(i + 1);
+        const sine_wave = math.sin(frequency * 2.0 * math.pi * sample_index / sample_rate) * gain;
+
+        // A number ranging from 0.0 to 1.0 in the first 1/64th of the duration of the tone.
+        const fade_in = @min(sample_index / (duration / 64.0), 1.0);
+
+        // A number ranging from 1.0 to 0.0 over half the duration of the tone.
+        const progression = sample_index / duration; // 0.0 (tone start) to 1.0 (tone end)
+        const fade_out = 1.0 - math.clamp(math.log10(progression * 10.0), 0.0, 1.0);
+
+        for (0..channels) |channel| {
+            samples[i + channel] = sine_wave * fade_in * fade_out;
+        }
+    }
+
+    return samples;
+}
+
+pub fn keyToFrequency(key: mach.core.Key) f32 {
+    // The frequencies here just come from a piano frequencies chart. You can google for them.
+    return switch (key) {
+        // First row of piano keys, the highest.
+        .q => 523.25, // C5
+        .w => 587.33, // D5
+        .e => 659.26, // E5
+        .r => 698.46, // F5
+        .t => 783.99, // G5
+        .y => 880.0, // A5
+        .u => 987.77, // B5
+        .i => 1046.5, // C6
+        .o => 1174.7, // D6
+        .p => 1318.5, // E6
+        .left_bracket => 1396.9, // F6
+        .right_bracket => 1568.0, // G6
+
+        // Second row of piano keys, the middle.
+        .a => 261.63, // C4
+        .s => 293.67, // D4
+        .d => 329.63, // E4
+        .f => 349.23, // F4
+        .g => 392.0, // G4
+        .h => 440.0, // A4
+        .j => 493.88, // B4
+        .k => 523.25, // C5
+        .l => 587.33, // D5
+        .semicolon => 659.26, // E5
+        .apostrophe => 698.46, // F5
+
+        // Third row of piano keys, the lowest.
+        .z => 130.81, // C3
+        .x => 146.83, // D3
+        .c => 164.81, // E3
+        .v => 174.61, // F3
+        .b => 196.00, // G3
+        .n => 220.0, // A3
+        .m => 246.94, // B3
+        .comma => 261.63, // C4
+        .period => 293.67, // D4
+        .slash => 329.63, // E5
+        else => 0.0,
+    };
+}
