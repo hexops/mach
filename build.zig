e9836485208a6fd37eb929b9a1d8524192694250
Stephen Gutekanst <stephen@hexops.com>
2024-01-14 19:12:40 -0700

    make Zig version check strict for Zig 2024.1.0-mach
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -184,8 +184,8 @@ fn sdkPath(comptime suffix: []const u8) []const u8 {
 }
 
 comptime {
-    const min_zig = std.SemanticVersion.parse("0.11.0") catch unreachable;
-    if (builtin.zig_version.order(min_zig) == .lt) {
-        @compileError(std.fmt.comptimePrint("Your Zig version v{} does not meet the minimum build requirement of v{}", .{ builtin.zig_version, min_zig }));
+    const supported_zig = std.SemanticVersion.parse("0.12.0-dev.2063+804cee3b9") catch unreachable;
+    if (builtin.zig_version.order(supported_zig) != .eq) {
+        @compileError(std.fmt.comptimePrint("unsupported Zig version ({}). Required Zig version 2024.1.0-mach: https://machengine.org/about/nominated-zig/#202410-mach", .{builtin.zig_version}));
     }
 }
