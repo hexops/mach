d19bc7fe978e1e089f6331992a72f32b0219c5d4
Stephen Gutekanst <stephen@hexops.com>
2024-03-10 12:18:19 -0700

    testing: export allocator/RefAllDeclsRecursive
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/testing.zig
+++ src/testing.zig
@@ -205,16 +205,19 @@ pub fn expect(comptime T: type, expected: T) Expect(T) {
     return Expect(T){ .expected = expected };
 }
 
+pub const allocator = testing.allocator;
+pub const refAllDeclsRecursive = testing.refAllDeclsRecursive;
+
 test {
-    testing.refAllDeclsRecursive(Expect(u32));
-    testing.refAllDeclsRecursive(Expect(f32));
-    testing.refAllDeclsRecursive(Expect([]const u8));
-    testing.refAllDeclsRecursive(Expect(@Vector(3, f32)));
-    testing.refAllDeclsRecursive(Expect(mach.math.Vec2h));
-    testing.refAllDeclsRecursive(Expect(mach.math.Vec3));
-    testing.refAllDeclsRecursive(Expect(mach.math.Vec4d));
-    testing.refAllDeclsRecursive(Expect(mach.math.Ray));
-    // testing.refAllDeclsRecursive(Expect(mach.math.Mat4h));
-    // testing.refAllDeclsRecursive(Expect(mach.math.Mat4));
-    // testing.refAllDeclsRecursive(Expect(mach.math.Mat4d));
+    refAllDeclsRecursive(Expect(u32));
+    refAllDeclsRecursive(Expect(f32));
+    refAllDeclsRecursive(Expect([]const u8));
+    refAllDeclsRecursive(Expect(@Vector(3, f32)));
+    refAllDeclsRecursive(Expect(mach.math.Vec2h));
+    refAllDeclsRecursive(Expect(mach.math.Vec3));
+    refAllDeclsRecursive(Expect(mach.math.Vec4d));
+    refAllDeclsRecursive(Expect(mach.math.Ray));
+    // refAllDeclsRecursive(Expect(mach.math.Mat4h));
+    // refAllDeclsRecursive(Expect(mach.math.Mat4));
+    // refAllDeclsRecursive(Expect(mach.math.Mat4d));
 }
