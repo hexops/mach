7ccfcefa9696d5d0b77ba643b1f681465b0b92a5
Stephen Gutekanst <stephen@hexops.com>
2024-05-06 13:56:36 -0700

    examples: update play-opus to latest Audio API
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- examples/play-opus/App.zig
+++ examples/play-opus/App.zig
@@ -20,6 +20,7 @@ pub const Mod = mach.Mod(@This());
 
 pub const events = .{
     .init = .{ .handler = init },
+    .after_init = .{ .handler = afterInit },
     .deinit = .{ .handler = deinit },
     .tick = .{ .handler = tick },
     .audio_state_change = .{ .handler = audioStateChange },
@@ -32,9 +33,8 @@ pub const components = .{
 sfx: Opus,
 
 fn init(core: *mach.Core.Mod, audio: *mach.Audio.Mod, app: *Mod) !void {
-    // Initialize audio module, telling it to send our module's .audio_state_change event when an
-    // entity's sound stops playing
-    audio.send(.init, .{app.event(.audio_state_change)});
+    audio.send(.init, .{});
+    app.send(.after_init, .{});
 
     const bgm_fbs = std.io.fixedBufferStream(assets.bgm.bit_bit_loop);
     const sfx_fbs = std.io.fixedBufferStream(assets.sfx.death);
@@ -61,6 +61,12 @@ fn init(core: *mach.Core.Mod, audio: *mach.Audio.Mod, app: *Mod) !void {
     std.debug.print("[arrow down] decrease volume 10%\n", .{});
 
     core.send(.start, .{});
+}
+
+fn afterInit(audio: *mach.Audio.Mod, app: *Mod) void {
+    // Configure the audio module to send our app's .audio_state_change event when an entity's sound
+    // finishes playing.
+    audio.state().on_state_change = app.event(.audio_state_change);
 }
 
 fn deinit(core: *mach.Core.Mod, audio: *mach.Audio.Mod) void {
