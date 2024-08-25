21c59b568f4189991caad07d67c2c6be59a807c4
Stephen Gutekanst <stephen@hexops.com>
2024-08-24 23:27:15 -0700

    core: remove linux support
    The `mach.Core` API needs major design changes, and every backend that it supports
    today must be updated to account for those API design changes. Unless someone is
    actively maintaining and improving the state of a given backend, it slows down our
    ability to make those critical changes.
    
    Unfortunately, the backends for X11 and Wayland today are half-baked, nobody is
    actively maintaining on or contributing to them, and the Linux CI tests have been
    broken for over a month as a result which harms overall stability of Mach.
    
    As a result, this PR removes Linux support from `mach.Core`
    
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- src/core/wayland/wayland.c
+++
@@ -1,7 +0,0 @@
-#include "wayland-client-protocol-code.h"
-#include "wayland-xdg-shell-client-protocol-code.h"
-#include "wayland-xdg-decoration-client-protocol-code.h"
-#include "wayland-viewporter-client-protocol-code.h"
-#include "wayland-relative-pointer-unstable-v1-client-protocol-code.h"
-#include "wayland-pointer-constraints-unstable-v1-client-protocol-code.h"
-#include "wayland-idle-inhibit-unstable-v1-client-protocol-code.h"
