f7141c01e2a6767e7c3540ce937fcae93bc1e2fa
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-09-10 16:19:37 -0700

    core: setup x11 shell

---
+++ src/core/linux/X11.zig
@@ -0,0 +1,25 @@
+const Linux = @import("../Linux.zig");
+const Core = @import("../../Core.zig");
+const InitOptions = Core.InitOptions;
+
+pub const X11 = @This();
+
+pub fn init(
+    linux: *Linux,
+    core: *Core.Mod,
+    options: InitOptions,
+) !X11 {
+    _ = linux;
+    _ = core;
+    _ = options;
+    // TODO(core): return errors.NotSupported if not supported
+    return .{};
+}
+
+pub fn deinit(
+    w: *X11,
+    linux: *Linux,
+) void {
+    _ = w;
+    _ = linux;
+}
